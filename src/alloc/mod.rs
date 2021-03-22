a6e468b81ecae8dfc744e67ad145de1dc417606b
Jorge C. Leitao <jorgecarleitao@gmail.com>
2021-03-22 05:48:31 +0000

    Added tests to alloc.

--- src/alloc/mod.rs
+++ src/alloc/mod.rs
@@ -130,11 +130,50 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_allocate_dangling() {
+        let p = allocate_aligned::<u32>(0);
+        assert_eq!(0, (p.as_ptr() as usize) % ALIGNMENT);
+    }
+
+    #[test]
     fn test_allocate() {
-        for _ in 0..10 {
-            let p = allocate_aligned::<u32>(1024);
-            assert_eq!(0, (p.as_ptr() as usize) % ALIGNMENT);
-            unsafe { free_aligned(p, 1024) };
-        }
+        let p = allocate_aligned::<u32>(1024);
+        assert_eq!(0, (p.as_ptr() as usize) % ALIGNMENT);
+        unsafe { free_aligned(p, 1024) };
+    }
+
+    #[test]
+    fn test_allocate_zeroed() {
+        let p = allocate_aligned_zeroed::<u32>(1024);
+        assert_eq!(0, (p.as_ptr() as usize) % ALIGNMENT);
+        unsafe { free_aligned(p, 1024) };
+    }
+
+    #[test]
+    fn test_reallocate_from_zero() {
+        let ptr = allocate_aligned::<u32>(0);
+        let ptr = unsafe { reallocate(ptr, 0, 512) };
+        unsafe { free_aligned(ptr, 512) };
+    }
+
+    #[test]
+    fn test_reallocate_from_alloc() {
+        let ptr = allocate_aligned::<u32>(32);
+        let ptr = unsafe { reallocate(ptr, 32, 64) };
+        unsafe { free_aligned(ptr, 64) };
+    }
+
+    #[test]
+    fn test_reallocate_smaller() {
+        let ptr = allocate_aligned::<u32>(32);
+        let ptr = unsafe { reallocate(ptr, 32, 16) };
+        unsafe { free_aligned(ptr, 16) };
+    }
+
+    #[test]
+    fn test_reallocate_to_zero() {
+        let ptr = allocate_aligned::<u32>(32);
+        let ptr = unsafe { reallocate(ptr, 32, 0) };
+        assert_eq!(ptr, unsafe { dangling() });
     }
 }
