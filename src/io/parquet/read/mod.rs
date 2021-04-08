4fd54959a33e9ee1ffadddc50147b47d708f3002
Jorge C. Leitao <jorgecarleitao@gmail.com>
2021-04-08 19:13:59 +0000

    First write.

--- src/io/parquet/read/mod.rs
+++ src/io/parquet/read/mod.rs
@@ -134,77 +134,8 @@ mod tests {
 
     use crate::array::*;
 
+    use super::super::tests::pyarrow_integration;
     use super::*;
-
-    fn pyarrow_integration(column: usize) -> Box<dyn Array> {
-        let i64_values = &[
-            Some(0),
-            Some(1),
-            None,
-            Some(3),
-            None,
-            Some(5),
-            Some(6),
-            Some(7),
-            None,
-            Some(9),
-        ];
-
-        match column {
-            0 => Box::new(Primitive::<i64>::from(i64_values).to(DataType::Int64)),
-            1 => Box::new(
-                Primitive::<f64>::from(&[
-                    Some(0.0),
-                    Some(1.0),
-                    None,
-                    Some(3.0),
-                    None,
-                    Some(5.0),
-                    Some(6.0),
-                    Some(7.0),
-                    None,
-                    Some(9.0),
-                ])
-                .to(DataType::Float64),
-            ),
-            2 => Box::new(Utf8Array::<i32>::from(&vec![
-                Some("Hello".to_string()),
-                None,
-                Some("aa".to_string()),
-                Some("".to_string()),
-                None,
-                Some("abc".to_string()),
-                None,
-                None,
-                Some("def".to_string()),
-                Some("aaa".to_string()),
-            ])),
-            3 => Box::new(BooleanArray::from(&[
-                Some(true),
-                None,
-                Some(false),
-                Some(false),
-                None,
-                Some(true),
-                None,
-                None,
-                Some(true),
-                Some(true),
-            ])),
-            4 => Box::new(
-                Primitive::<i64>::from(i64_values)
-                    .to(DataType::Timestamp(TimeUnit::Millisecond, None)),
-            ),
-            5 => {
-                let values = i64_values
-                    .iter()
-                    .map(|x| x.map(|x| x as u32))
-                    .collect::<Vec<_>>();
-                Box::new(Primitive::<u32>::from(values).to(DataType::UInt32))
-            }
-            _ => unreachable!(),
-        }
-    }
 
     fn get_column(path: &str, row_group: usize, column: usize) -> Result<Box<dyn Array>> {
         let mut file = File::open(path).unwrap();
---
+++ src/io/parquet/write/mod.rs
@@ -0,0 +1,99 @@
+mod primitive;
+
+use parquet2::{compression::CompressionCodec, read::CompressedPage};
+
+use crate::array::*;
+use crate::datatypes::DataType;
+use crate::error::Result;
+
+pub fn array_to_page(array: &dyn Array) -> Result<CompressedPage> {
+    // using plain encoding format
+    let compression = CompressionCodec::Uncompressed;
+    match array.data_type() {
+        DataType::Int32 => {
+            primitive::array_to_page_v1::<i32>(array.as_any().downcast_ref().unwrap(), compression)
+        }
+        DataType::Int64 => {
+            primitive::array_to_page_v1::<i64>(array.as_any().downcast_ref().unwrap(), compression)
+        }
+        DataType::Float32 => {
+            primitive::array_to_page_v1::<f32>(array.as_any().downcast_ref().unwrap(), compression)
+        }
+        DataType::Float64 => {
+            primitive::array_to_page_v1::<f64>(array.as_any().downcast_ref().unwrap(), compression)
+        }
+        _ => todo!(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::{Cursor, Read, Seek};
+
+    use super::*;
+
+    use parquet2::{
+        metadata::SchemaDescriptor,
+        read::{get_page_iterator, read_metadata},
+        schema::io_message::from_message,
+        write::write_file,
+    };
+
+    use crate::error::Result;
+    use crate::io::parquet::read::page_iter_to_array;
+
+    use super::super::tests::pyarrow_integration;
+
+    fn read_column<R: Read + Seek>(
+        reader: &mut R,
+        row_group: usize,
+        column: usize,
+    ) -> Result<Box<dyn Array>> {
+        let metadata = read_metadata(reader)?;
+        let iter = get_page_iterator(&metadata, row_group, column, reader)?;
+
+        let descriptor = iter.descriptor().clone();
+
+        page_iter_to_array(iter, &descriptor)
+    }
+
+    fn round_trip(column: usize) -> Result<()> {
+        let array = pyarrow_integration(column);
+
+        let row_groups = std::iter::once(Result::Ok(std::iter::once(Ok(std::iter::once(
+            array_to_page(array.as_ref()),
+        )))));
+
+        // prepare schema
+        let a = match array.data_type() {
+            DataType::Int32 => "INT32",
+            DataType::Int64 => "INT64",
+            DataType::Float32 => "FLOAT",
+            DataType::Float64 => "DOUBLE",
+            _ => todo!(),
+        };
+        let schema = SchemaDescriptor::new(from_message(&format!(
+            "message schema {{ OPTIONAL {} col; }}",
+            a
+        ))?);
+
+        let mut writer = Cursor::new(vec![]);
+        write_file(
+            &mut writer,
+            &schema,
+            CompressionCodec::Uncompressed,
+            row_groups,
+        )?;
+
+        let data = writer.into_inner();
+
+        let result = read_column(&mut Cursor::new(data), 0, 0)?;
+        assert_eq!(array.as_ref(), result.as_ref());
+        Ok(())
+    }
+
+    #[test]
+    fn test_int32() -> Result<()> {
+        round_trip(0)
+    }
+}
