a4835ca04f58207bcfe98cf4fae3a9b21b70ab08
Fernando Herrera <fernando.j.herrera@gmail.com>
2021-04-18 08:09:50 +0100

    Suite of functions for basic multiplication

--- src/compute/arithmetics/basic/mul.rs
+++ src/compute/arithmetics/basic/mul.rs
@@ -1,12 +1,33 @@
 use std::ops::Mul;
 
+use num::{
+    traits::{ops::overflowing::OverflowingMul, SaturatingMul},
+    CheckedMul,
+};
+
 use crate::{
     array::{Array, PrimitiveArray},
-    compute::arity::{binary, unary},
+    bitmap::Bitmap,
+    compute::arity::{binary, binary_with_bitmap, try_binary, try_unary, unary, unary_with_bitmap},
     error::{ArrowError, Result},
     types::NativeType,
 };
 
+/// Multiplies two primitive arrays with the same type.
+/// Panics if the multiplication of one pair of values overflows.
+///
+/// # Examples
+/// ```
+/// use arrow2::compute::arithmetics::basic::mul::mul;
+/// use arrow2::array::Primitive;
+/// use arrow2::datatypes::DataType;
+///
+/// let a = Primitive::from(&vec![None, Some(6), None, Some(6)]).to(DataType::Int32);
+/// let b = Primitive::from(&vec![Some(5), None, None, Some(6)]).to(DataType::Int32);
+/// let result = mul(&a, &b).unwrap();
+/// let expected = Primitive::from(&vec![None, None, None, Some(36)]).to(DataType::Int32);
+/// assert_eq!(result, expected)
+/// ```
 #[inline]
 pub fn mul<T>(lhs: &PrimitiveArray<T>, rhs: &PrimitiveArray<T>) -> Result<PrimitiveArray<T>>
 where
@@ -18,16 +39,231 @@ where
         ));
     }
 
-    binary(lhs, rhs, lhs.data_type().clone(), |lhs, rhs| lhs * rhs)
+    binary(lhs, rhs, lhs.data_type().clone(), |a, b| a * b)
 }
 
+/// Checked multiplication of two primitive arrays. If the result from the
+/// multiplications overflows for this type an ArrowError::ArithmeticError is
+/// returned.
+///
+/// # Examples
+/// ```
+/// use arrow2::compute::arithmetics::basic::mul::checked_mul;
+/// use arrow2::array::Primitive;
+/// use arrow2::datatypes::DataType;
+///
+/// let a = Primitive::from(&vec![Some(100i8)]).to(DataType::Int8);
+/// let b = Primitive::from(&vec![Some(100i8)]).to(DataType::Int8);
+/// checked_mul(&a, &b)
+///     .err()
+///     .expect("should have failed due to overflow");
+/// ```
+pub fn checked_mul<T>(lhs: &PrimitiveArray<T>, rhs: &PrimitiveArray<T>) -> Result<PrimitiveArray<T>>
+where
+    T: NativeType + CheckedMul<Output = T>,
+{
+    if lhs.data_type() != rhs.data_type() {
+        return Err(ArrowError::InvalidArgumentError(
+            "Arrays must have the same logical type".to_string(),
+        ));
+    }
+
+    let op = move |a: T, b: T| {
+        let res = a.checked_mul(&b);
+
+        match res {
+            Some(val) => Ok(val),
+            None => {
+                return Err(ArrowError::ArithmeticError(
+                    "Saturated result in multiplication".to_string(),
+                ));
+            }
+        }
+    };
+
+    try_binary(lhs, rhs, lhs.data_type().clone(), op)
+}
+
+/// Saturating multiplication of two primitive arrays. If the result from the
+/// multiplication overflows, the result for the
+/// operation will be the saturated value.
+///
+/// # Examples
+/// ```
+/// use arrow2::compute::arithmetics::basic::mul::saturating_mul;
+/// use arrow2::array::Primitive;
+/// use arrow2::datatypes::DataType;
+///
+/// let a = Primitive::from(&vec![Some(-100i8)]).to(DataType::Int8);
+/// let b = Primitive::from(&vec![Some(100i8)]).to(DataType::Int8);
+/// let result = saturating_mul(&a, &b).unwrap();
+/// let expected = Primitive::from(&vec![Some(-128)]).to(DataType::Int8);
+/// assert_eq!(result, expected);
+/// ```
+pub fn saturating_mul<T>(
+    lhs: &PrimitiveArray<T>,
+    rhs: &PrimitiveArray<T>,
+) -> Result<PrimitiveArray<T>>
+where
+    T: NativeType + SaturatingMul<Output = T>,
+{
+    if lhs.data_type() != rhs.data_type() {
+        return Err(ArrowError::InvalidArgumentError(
+            "Arrays must have the same logical type".to_string(),
+        ));
+    }
+
+    let op = move |a: T, b: T| a.saturating_mul(&b);
+
+    binary(lhs, rhs, lhs.data_type().clone(), op)
+}
+
+/// Overflowing multiplication of two primitive arrays. If the result from the
+/// mul overflows, the result for the operation will be an array with
+/// overflowed values and a validity array indicating the overflowing elements
+/// from the array.
+///
+/// # Examples
+/// ```
+/// use arrow2::compute::arithmetics::basic::mul::overflowing_mul;
+/// use arrow2::array::Primitive;
+/// use arrow2::datatypes::DataType;
+///
+/// let a = Primitive::from(&vec![Some(1i8), Some(-100i8)]).to(DataType::Int8);
+/// let b = Primitive::from(&vec![Some(1i8), Some(100i8)]).to(DataType::Int8);
+/// let (result, overflow) = overflowing_mul(&a, &b).unwrap();
+/// let expected = Primitive::from(&vec![Some(1i8), Some(-16i8)]).to(DataType::Int8);
+/// assert_eq!(result, expected);
+/// ```
+pub fn overflowing_mul<T>(
+    lhs: &PrimitiveArray<T>,
+    rhs: &PrimitiveArray<T>,
+) -> Result<(PrimitiveArray<T>, Bitmap)>
+where
+    T: NativeType + OverflowingMul<Output = T>,
+{
+    if lhs.data_type() != rhs.data_type() {
+        return Err(ArrowError::InvalidArgumentError(
+            "Arrays must have the same logical type".to_string(),
+        ));
+    }
+
+    let op = move |a: T, b: T| a.overflowing_mul(&b);
+
+    binary_with_bitmap(lhs, rhs, lhs.data_type().clone(), op)
+}
+
+/// Multiply a scalar T to a primitive array of type T.
+/// Panics if the multiplication of the values overflows.
+///
+/// # Examples
+/// ```
+/// use arrow2::compute::arithmetics::basic::mul::mul_scalar;
+/// use arrow2::array::Primitive;
+/// use arrow2::datatypes::DataType;
+///
+/// let a = Primitive::from(&vec![None, Some(6), None, Some(6)]).to(DataType::Int32);
+/// let result = mul_scalar(&a, &2i32);
+/// let expected = Primitive::from(&vec![None, Some(12), None, Some(12)]).to(DataType::Int32);
+/// assert_eq!(result, expected)
+/// ```
 #[inline]
 pub fn mul_scalar<T>(lhs: &PrimitiveArray<T>, rhs: &T) -> PrimitiveArray<T>
 where
     T: NativeType + Mul<Output = T>,
 {
     let rhs = *rhs;
-    unary(lhs, |lhs| lhs * rhs, lhs.data_type())
+    unary(lhs, |a| a * rhs, lhs.data_type())
+}
+
+/// Checked multiplication of a scalar T to a primitive array of type T. If the
+/// result from the multiplication overflows for this
+/// type an ArrowError::ArithmeticError is returned.
+///
+/// # Examples
+/// ```
+/// use arrow2::compute::arithmetics::basic::mul::checked_mul_scalar;
+/// use arrow2::array::Primitive;
+/// use arrow2::datatypes::DataType;
+///
+/// let a = Primitive::from(&vec![Some(-100i8)]).to(DataType::Int8);
+/// checked_mul_scalar(&a, &100i8)
+///     .err()
+///     .expect("should have failed due to overflow");
+/// ```
+#[inline]
+pub fn checked_mul_scalar<T>(lhs: &PrimitiveArray<T>, rhs: &T) -> Result<PrimitiveArray<T>>
+where
+    T: NativeType + CheckedMul<Output = T>,
+{
+    let rhs = *rhs;
+    let op = move |a: T| {
+        let res = a.checked_mul(&rhs);
+
+        match res {
+            Some(val) => Ok(val),
+            None => {
+                return Err(ArrowError::ArithmeticError(
+                    "Saturated result in multiplication".to_string(),
+                ));
+            }
+        }
+    };
+
+    try_unary(lhs, op, lhs.data_type())
+}
+
+/// Saturated multiplication of a scalar T to a primitive array of type T. If the
+/// result from the mul overflows for this type, then
+/// the result will be saturated
+///
+/// # Examples
+/// ```
+/// use arrow2::compute::arithmetics::basic::mul::saturating_mul_scalar;
+/// use arrow2::array::Primitive;
+/// use arrow2::datatypes::DataType;
+///
+/// let a = Primitive::from(&vec![Some(-100i8)]).to(DataType::Int8);
+/// let result = saturating_mul_scalar(&a, &100i8);
+/// let expected = Primitive::from(&vec![Some(-128i8)]).to(DataType::Int8);
+/// assert_eq!(result, expected);
+/// ```
+#[inline]
+pub fn saturating_mul_scalar<T>(lhs: &PrimitiveArray<T>, rhs: &T) -> PrimitiveArray<T>
+where
+    T: NativeType + SaturatingMul<Output = T>,
+{
+    let rhs = *rhs;
+    let op = move |a: T| a.saturating_mul(&rhs);
+
+    unary(lhs, op, lhs.data_type())
+}
+
+/// Overflowing multiplication of a scalar T to a primitive array of type T. If
+/// the result from the mul overflows for this type,
+/// then the result will be an array with overflowed values and a validity
+/// array indicating the overflowing elements from the array
+///
+/// # Examples
+/// ```
+/// use arrow2::compute::arithmetics::basic::mul::overflowing_mul_scalar;
+/// use arrow2::array::Primitive;
+/// use arrow2::datatypes::DataType;
+///
+/// let a = Primitive::from(&vec![Some(1i8), Some(100i8)]).to(DataType::Int8);
+/// let (result, overflow) = overflowing_mul_scalar(&a, &100i8);
+/// let expected = Primitive::from(&vec![Some(100i8), Some(16i8)]).to(DataType::Int8);
+/// assert_eq!(result, expected);
+/// ```
+#[inline]
+pub fn overflowing_mul_scalar<T>(lhs: &PrimitiveArray<T>, rhs: &T) -> (PrimitiveArray<T>, Bitmap)
+where
+    T: NativeType + OverflowingMul<Output = T>,
+{
+    let rhs = *rhs;
+    let op = move |a: T| a.overflowing_mul(&rhs);
+
+    unary_with_bitmap(lhs, op, lhs.data_type())
 }
 
 #[cfg(test)]
@@ -37,11 +273,124 @@ mod tests {
     use crate::datatypes::DataType;
 
     #[test]
-    fn test_multiply() {
+    fn test_mul_mismatched_length() {
+        let a = Primitive::from_slice(vec![5, 6]).to(DataType::Int32);
+        let b = Primitive::from_slice(vec![5]).to(DataType::Int32);
+        mul(&a, &b)
+            .err()
+            .expect("should have failed due to different lengths");
+    }
+
+    #[test]
+    fn test_mul() {
         let a = Primitive::from(&vec![None, Some(6), None, Some(6)]).to(DataType::Int32);
         let b = Primitive::from(&vec![Some(5), None, None, Some(6)]).to(DataType::Int32);
         let result = mul(&a, &b).unwrap();
         let expected = Primitive::from(&vec![None, None, None, Some(36)]).to(DataType::Int32);
         assert_eq!(result, expected)
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_mul_panic() {
+        let a = Primitive::from(&vec![Some(-100i8)]).to(DataType::Int8);
+        let b = Primitive::from(&vec![Some(100i8)]).to(DataType::Int8);
+        let _ = mul(&a, &b);
+    }
+
+    #[test]
+    fn test_mul_checked() {
+        let a = Primitive::from(&vec![None, Some(6), None, Some(6)]).to(DataType::Int32);
+        let b = Primitive::from(&vec![Some(5), None, None, Some(6)]).to(DataType::Int32);
+        let result = checked_mul(&a, &b).unwrap();
+        let expected = Primitive::from(&vec![None, None, None, Some(36)]).to(DataType::Int32);
+        assert_eq!(result, expected);
+
+        let a = Primitive::from(&vec![Some(-100i8)]).to(DataType::Int8);
+        let b = Primitive::from(&vec![Some(100i8)]).to(DataType::Int8);
+        checked_mul(&a, &b)
+            .err()
+            .expect("should have failed due to overflow");
+    }
+
+    #[test]
+    fn test_mul_saturating() {
+        let a = Primitive::from(&vec![None, Some(6), None, Some(6)]).to(DataType::Int32);
+        let b = Primitive::from(&vec![Some(5), None, None, Some(6)]).to(DataType::Int32);
+        let result = saturating_mul(&a, &b).unwrap();
+        let expected = Primitive::from(&vec![None, None, None, Some(36)]).to(DataType::Int32);
+        assert_eq!(result, expected);
+
+        let a = Primitive::from(&vec![Some(-100i8)]).to(DataType::Int8);
+        let b = Primitive::from(&vec![Some(100i8)]).to(DataType::Int8);
+        let result = saturating_mul(&a, &b).unwrap();
+        let expected = Primitive::from(&vec![Some(-128)]).to(DataType::Int8);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_mul_overflowing() {
+        let a = Primitive::from(&vec![None, Some(6), None, Some(6)]).to(DataType::Int32);
+        let b = Primitive::from(&vec![Some(5), None, None, Some(6)]).to(DataType::Int32);
+        let (result, overflow) = overflowing_mul(&a, &b).unwrap();
+        let expected = Primitive::from(&vec![None, None, None, Some(36)]).to(DataType::Int32);
+        assert_eq!(result, expected);
+        assert_eq!(overflow.as_slice()[0], 0b0000);
+
+        let a = Primitive::from(&vec![Some(1i8), Some(-100i8)]).to(DataType::Int8);
+        let b = Primitive::from(&vec![Some(1i8), Some(100i8)]).to(DataType::Int8);
+        let (result, overflow) = overflowing_mul(&a, &b).unwrap();
+        let expected = Primitive::from(&vec![Some(1i8), Some(-16i8)]).to(DataType::Int8);
+        assert_eq!(result, expected);
+        assert_eq!(overflow.as_slice()[0], 0b10);
+    }
+
+    #[test]
+    fn test_mul_scalar() {
+        let a = Primitive::from(&vec![None, Some(6), None, Some(6)]).to(DataType::Int32);
+        let result = mul_scalar(&a, &1i32);
+        let expected = Primitive::from(&vec![None, Some(6), None, Some(6)]).to(DataType::Int32);
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn test_mul_scalar_checked() {
+        let a = Primitive::from(&vec![None, Some(6), None, Some(6)]).to(DataType::Int32);
+        let result = checked_mul_scalar(&a, &1i32).unwrap();
+        let expected = Primitive::from(&vec![None, Some(6), None, Some(6)]).to(DataType::Int32);
+        assert_eq!(result, expected);
+
+        let a = Primitive::from(&vec![Some(-100i8)]).to(DataType::Int8);
+        checked_mul_scalar(&a, &100i8)
+            .err()
+            .expect("should have failed due to overflow");
+    }
+
+    #[test]
+    fn test_mul_scalar_saturating() {
+        let a = Primitive::from(&vec![None, Some(6), None, Some(6)]).to(DataType::Int32);
+        let result = saturating_mul_scalar(&a, &1i32);
+        let expected = Primitive::from(&vec![None, Some(6), None, Some(6)]).to(DataType::Int32);
+        assert_eq!(result, expected);
+
+        let a = Primitive::from(&vec![Some(-100i8)]).to(DataType::Int8);
+        let result = saturating_mul_scalar(&a, &100i8);
+        let expected = Primitive::from(&vec![Some(-128)]).to(DataType::Int8);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_mul_scalar_overflowing() {
+        let a = Primitive::from(&vec![None, Some(6), None, Some(6)]).to(DataType::Int32);
+        let (result, overflow) = overflowing_mul_scalar(&a, &1i32);
+        let expected = Primitive::from(&vec![None, Some(6), None, Some(6)]).to(DataType::Int32);
+        assert_eq!(result, expected);
+        assert_eq!(overflow.as_slice()[0], 0b0000);
+
+        let a = Primitive::from(&vec![Some(1i8), Some(-100i8)]).to(DataType::Int8);
+        let (result, overflow) = overflowing_mul_scalar(&a, &100i8);
+        let expected = Primitive::from(&vec![Some(100i8), Some(-16i8)]).to(DataType::Int8);
+        assert_eq!(result, expected);
+        assert_eq!(overflow.as_slice()[0], 0b10);
     }
 }
