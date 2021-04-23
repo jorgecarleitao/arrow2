3c3edc7cea6a48e375afa1301c15a4a4588f37ce
Jorge C. Leitao <jorgecarleitao@gmail.com>
2021-04-23 04:20:30 +0000

    Added bench for min.

--- benches/aggregate.rs
+++ benches/aggregate.rs
@@ -4,13 +4,17 @@ use criterion::{criterion_group, criterion_main, Criterion};
 
 use arrow2::array::*;
 use arrow2::util::bench_util::*;
-use arrow2::{compute::aggregate::sum, datatypes::DataType, types::NativeType};
+use arrow2::{compute::aggregate::*, datatypes::DataType, types::NativeType};
 
-fn bench_op<T>(arr_a: &PrimitiveArray<T>)
+fn bench_sum<T>(arr_a: &PrimitiveArray<T>)
 where
     T: NativeType + std::iter::Sum + AddAssign,
 {
     sum(criterion::black_box(arr_a)).unwrap();
+}
+
+fn bench_min(arr_a: &PrimitiveArray<f32>) {
+    min_f32(criterion::black_box(arr_a)).unwrap();
 }
 
 fn add_benchmark(c: &mut Criterion) {
@@ -18,6 +22,8 @@ fn add_benchmark(c: &mut Criterion) {
     let arr_a = create_primitive_array::<f32>(size, DataType::Float32, 0.0);
 
     c.bench_function("add f32", |b| b.iter(|| bench_op(&arr_a)));
+
+    c.bench_function("min f32", |b| b.iter(|| bench_min(&arr_a)));
 
     let arr_a = create_primitive_array::<f32>(size, DataType::Float32, 0.5);
 
