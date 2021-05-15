d4e208a805c01dc225d9bb4f736021dc38c0b364
Jorge C. Leitao <jorgecarleitao@gmail.com>
2021-05-15 08:26:56 +0000

    Added tests.

--- src/compute/cast/mod.rs
+++ src/compute/cast/mod.rs
@@ -889,6 +889,55 @@ mod tests {
         cast(&array, &DataType::Timestamp(TimeUnit::Microsecond, None)).unwrap();
     }
 
+    #[test]
+    fn consistency() {
+        use crate::datatypes::DataType::*;
+        let datatypes = vec![
+            Null,
+            Boolean,
+            UInt8,
+            UInt16,
+            UInt32,
+            UInt64,
+            Int8,
+            Int16,
+            Int32,
+            Int64,
+            Float32,
+            Float64,
+            Timestamp(TimeUnit::Second, None),
+            Timestamp(TimeUnit::Millisecond, None),
+            Timestamp(TimeUnit::Microsecond, None),
+            Timestamp(TimeUnit::Nanosecond, None),
+            Time64(TimeUnit::Microsecond),
+            Time64(TimeUnit::Nanosecond),
+            Date32,
+            Time32(TimeUnit::Second),
+            Time32(TimeUnit::Millisecond),
+            Date64,
+            Utf8,
+            LargeUtf8,
+            Binary,
+            LargeBinary,
+            Duration(TimeUnit::Second),
+            Duration(TimeUnit::Millisecond),
+            Duration(TimeUnit::Microsecond),
+            Duration(TimeUnit::Nanosecond),
+        ];
+        datatypes
+            .clone()
+            .into_iter()
+            .zip(datatypes.into_iter())
+            .for_each(|(d1, d2)| {
+                let array = new_null_array(d1.clone(), 10);
+                if can_cast_types(&d1, &d2) {
+                    assert!(cast(array.as_ref(), &d2).is_ok());
+                } else {
+                    assert!(cast(array.as_ref(), &d2).is_err());
+                }
+            });
+    }
+
     /*
     #[test]
     fn test_cast_list_i32_to_list_u16() {
