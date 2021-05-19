9e4c3c3d8972fb3a5d876c87a95d1bf3cf2a2a2c
Jorge C. Leitao <jorgecarleitao@gmail.com>
2021-05-19 20:49:13 +0000

    Impemented TrustedLen for RepeatWith.

--- src/trusted_len.rs
+++ src/trusted_len.rs
@@ -40,4 +40,5 @@ unsafe impl<T> TrustedLen for std::iter::Once<T> {}
 unsafe impl<T> TrustedLen for std::vec::IntoIter<T> {}
 
 unsafe impl<A: Clone> TrustedLen for std::iter::Repeat<A> {}
+unsafe impl<A, F: FnMut() -> A> TrustedLen for std::iter::RepeatWith<F> {}
 unsafe impl<A: TrustedLen> TrustedLen for std::iter::Take<A> {}
