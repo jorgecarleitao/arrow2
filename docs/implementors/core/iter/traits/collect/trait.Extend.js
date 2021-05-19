d7705fd29dfd272fa9d416762677646fe913da9e
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2021-05-19 20:52:43 +0000

    deploy: 9e4c3c3d8972fb3a5d876c87a95d1bf3cf2a2a2c

---
+++ docs/implementors/core/iter/traits/collect/trait.Extend.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["arrow2"] = [{"text":"impl&lt;A:&nbsp;<a class=\"trait\" href=\"arrow2/types/trait.NativeType.html\" title=\"trait arrow2::types::NativeType\">NativeType</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.Extend.html\" title=\"trait core::iter::traits::collect::Extend\">Extend</a>&lt;A&gt; for <a class=\"struct\" href=\"arrow2/buffer/struct.MutableBuffer.html\" title=\"struct arrow2::buffer::MutableBuffer\">MutableBuffer</a>&lt;A&gt;","synthetic":false,"types":["arrow2::buffer::mutable::MutableBuffer"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
