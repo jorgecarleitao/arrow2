560aa0e723ea854928a89a32ff3bbf36394c4c63
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2021-05-26 19:17:02 +0000

    deploy: f40451cb7f36347db1233c0c7ab180e0a95e9a85

---
+++ docs/arrow2/types/simd/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["f32x16",""],["f64x8",""],["i16x32",""],["i32x16",""],["i64x8",""],["i8x64",""],["u16x32",""],["u32x16",""],["u64x8",""],["u8x64",""]],"trait":[["FromMaskChunk",""],["NativeSimd","A struct lends itself well to be compiled leveraging SIMD"],["Simd","Trait NativeTypes that have a representation for SIMD instructions."]]});
