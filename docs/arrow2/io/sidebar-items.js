e486eb2622082cf269e150578327da2c7a0887fb
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2021-06-13 07:30:31 +0000

    deploy: bfa8819ef0b4e607d6ec773bf1aaef3b03b7f8a6

---
+++ docs/arrow2/io/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["csv","Transfer data between the Arrow memory format and CSV (comma-separated values)."],["ipc",""],["json","Convert data between the Arrow memory format and JSON line-delimited records."],["json_integration","Utils for JSON integration testing"],["parquet",""],["print",""]]});
