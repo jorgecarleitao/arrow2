7adb31709f3e8a15299facc8d288cfcbdbf8cdbb
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2021-06-22 17:30:05 +0000

    deploy: c77ce6f5fcf145e0882cd1e3c536a05ec2d13139

--- docs/arrow2/io/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"mod":[["csv","Transfer data between the Arrow memory format and CSV (comma-separated values)."],["ipc",""],["json","Convert data between the Arrow memory format and JSON line-delimited records."],["json_integration","Utils for JSON integration testing"],["parquet",""],["print",""]]});
--- docs/arrow2/types/simd/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["f32x16",""],["f64x8",""],["i16x32",""],["i32x16",""],["i64x8",""],["i8x64",""],["u16x32",""],["u32x16",""],["u64x8",""],["u8x64",""]],"trait":[["FromMaskChunk",""],["NativeSimd","A struct lends itself well to be compiled leveraging SIMD"],["Simd","Trait implemented by some [`NativeType`] that have a SIMD representation."]]});
