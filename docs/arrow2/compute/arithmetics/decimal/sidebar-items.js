0c8fac909bc16d3590dbf7959e6fccd6959391a8
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2021-06-27 08:32:14 +0000

    deploy: 126ca868e91abcd4cdd957a2e48072fa840859c3

--- docs/arrow2/compute/arithmetics/decimal/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"mod":[["add","Defines the addition arithmetic kernels for Decimal `PrimitiveArrays`."],["div","Defines the division arithmetic kernels for Decimal `PrimitiveArrays`."],["mul","Defines the multiplication arithmetic kernels for Decimal `PrimitiveArrays`."],["sub","Defines the subtract arithmetic kernels for Decimal `PrimitiveArrays`."]]});
