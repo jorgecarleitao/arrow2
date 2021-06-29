6d171ada9ac4c2d37227221668671e38430dec85
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2021-06-29 15:43:41 +0000

    deploy: 3a6920360c60d94bcb356981a5fd95d811a425f0

---
+++ docs/arrow2/compute/length/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["can_length","Checks if an array of type `datatype` can perform length operation"],["length","Returns an array of integers with the number of bytes on each string of the array."]]});
