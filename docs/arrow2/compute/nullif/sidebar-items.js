4413c9104f81cb939688b1f69d144115fc26d4c3
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2021-07-01 15:32:07 +0000

    deploy: 1e54fd43ce9ce78958f9243a137acf15b3b57f5e

--- docs/arrow2/compute/nullif/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"fn":[["can_nullif","Returns whether [`nulliff`] is implemented for the datatypes."],["nullif","Returns an array whose validity is null iff `lhs == rhs` or `lhs` is null. This has the same semantics as postgres."],["nullif_primitive","Returns an array whose validity is null iff `lhs == rhs` or `lhs` is null. This has the same semantics as postgres."]]});
