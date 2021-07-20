74737d39c33b5cd8365eee2da84ee4e4ed317ebf
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2021-07-20 21:35:25 +0000

    deploy: 20072a558821bce49f74367c875a544c84e6f410

--- docs/arrow2/compute/take/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"fn":[["can_take","Checks if an array of type `datatype` can perform take operation"],["take",""]],"trait":[["Index","Trait describing any type that can be used to index a slot of an array."]]});
