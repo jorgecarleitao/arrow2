07942f1c891c672c3d8457df53d1d7f8ea62da60
Jorge C. Leitao <jorgecarleitao@gmail.com>
2021-07-27 13:55:34 +0000

    Simplified branching of parquet reader.

--- src/io/parquet/read/mod.rs
+++ src/io/parquet/read/mod.rs
@@ -12,7 +12,7 @@ mod utils;
 
 use crate::{
     array::Array,
-    datatypes::{DataType, TimeUnit},
+    datatypes::{DataType, IntervalUnit, TimeUnit},
     error::{ArrowError, Result},
 };
 
@@ -23,17 +23,13 @@ pub use parquet2::{
     error::ParquetError,
     metadata::{ColumnChunkMetaData, ColumnDescriptor, RowGroupMetaData},
     read::{
-        decompress, streaming_iterator, CompressedPage, Decompressor, Page, PageHeader,
+        decompress, get_page_iterator as _get_page_iterator, read_metadata as _read_metadata,
+        streaming_iterator, CompressedPage, Decompressor, Page, PageHeader, PageIterator,
         StreamingIterator,
     },
     schema::{
         types::{LogicalType, ParquetType, PhysicalType, PrimitiveConvertedType},
         TimeUnit as ParquetTimeUnit, TimestampType,
-    },
-};
-use parquet2::{
-    read::{
-        get_page_iterator as _get_page_iterator, read_metadata as _read_metadata, PageIterator,
     },
     types::int96_to_i64_ns,
 };
@@ -56,185 +52,98 @@ pub fn read_metadata<R: Read + Seek>(reader: &mut R) -> Result<FileMetaData> {
     Ok(_read_metadata(reader)?)
 }
 
-fn page_iter_i64<I: StreamingIterator<Item = std::result::Result<Page, ParquetError>>>(
-    iter: I,
-    metadata: &ColumnChunkMetaData,
-    data_type: DataType,
-) -> Result<Box<dyn Array>> {
-    if metadata.descriptor().max_rep_level() > 0 {
-        let inner = match data_type {
-            DataType::List(ref inner) => inner.data_type(),
-            DataType::LargeList(ref inner) => inner.data_type(),
-            _ => unimplemented!(),
-        };
-
-        match inner {
-            DataType::UInt64 => {
-                primitive::iter_to_array_nested(iter, metadata, data_type, |x: i64| x as u64)
-            }
-            _ => primitive::iter_to_array_nested(iter, metadata, data_type, |x: i64| x as i64),
-        }
-    } else {
-        match data_type {
-            DataType::UInt64 => {
-                primitive::iter_to_array(iter, metadata, data_type, |x: i64| x as u64)
-            }
-            _ => primitive::iter_to_array(iter, metadata, data_type, |x: i64| x as i64),
-        }
-    }
-}
-
-fn page_iter_i32<I: StreamingIterator<Item = std::result::Result<Page, ParquetError>>>(
-    iter: I,
-    metadata: &ColumnChunkMetaData,
-    data_type: DataType,
-) -> Result<Box<dyn Array>> {
-    use DataType::*;
-
-    if metadata.descriptor().max_rep_level() > 0 {
-        let inner = match data_type {
-            List(ref inner) => inner.data_type(),
-            LargeList(ref inner) => inner.data_type(),
-            _ => unimplemented!(),
-        };
-
-        match inner {
-            UInt8 => primitive::iter_to_array_nested(iter, metadata, data_type, |x: i32| x as u8),
-            UInt16 => primitive::iter_to_array_nested(iter, metadata, data_type, |x: i32| x as u16),
-            UInt32 => primitive::iter_to_array_nested(iter, metadata, data_type, |x: i32| x as u32),
-            Int8 => primitive::iter_to_array_nested(iter, metadata, data_type, |x: i32| x as i8),
-            Int16 => primitive::iter_to_array_nested(iter, metadata, data_type, |x: i32| x as i16),
-            _ => primitive::iter_to_array_nested(iter, metadata, data_type, |x: i32| x),
-        }
-    } else {
-        match data_type {
-            UInt8 => primitive::iter_to_array(iter, metadata, data_type, |x: i32| x as u8),
-            UInt16 => primitive::iter_to_array(iter, metadata, data_type, |x: i32| x as u16),
-            UInt32 => primitive::iter_to_array(iter, metadata, data_type, |x: i32| x as u32),
-            Int8 => primitive::iter_to_array(iter, metadata, data_type, |x: i32| x as i8),
-            Int16 => primitive::iter_to_array(iter, metadata, data_type, |x: i32| x as i16),
-            _ => primitive::iter_to_array(iter, metadata, data_type, |x: i32| x),
-        }
-    }
-}
-
-fn page_iter_byte_array<I: StreamingIterator<Item = std::result::Result<Page, ParquetError>>>(
-    iter: I,
-    metadata: &ColumnChunkMetaData,
-    data_type: DataType,
-) -> Result<Box<dyn Array>> {
-    use DataType::*;
-
-    if metadata.descriptor().max_rep_level() > 0 {
-        let inner = match data_type {
-            List(ref inner) => inner.data_type(),
-            LargeList(ref inner) => inner.data_type(),
-            _ => unimplemented!(),
-        };
-
-        match inner {
-            Binary | Utf8 => binary::iter_to_array_nested::<i32, _, _>(iter, metadata, data_type),
-            LargeBinary | LargeUtf8 => {
-                binary::iter_to_array_nested::<i64, _, _>(iter, metadata, data_type)
-            }
-            other => Err(ArrowError::NotYetImplemented(format!(
-                "Can't read {:?} from parquet",
-                other
-            ))),
-        }
-    } else {
-        match data_type {
-            Binary | Utf8 => binary::iter_to_array::<i32, _, _>(iter, metadata, &data_type),
-            LargeBinary | LargeUtf8 => {
-                binary::iter_to_array::<i64, _, _>(iter, metadata, &data_type)
-            }
-            other => Err(ArrowError::NotYetImplemented(format!(
-                "Can't read {:?} from parquet",
-                other
-            ))),
-        }
-    }
-}
-
-fn page_iter_fixed_len_byte_array<
-    I: StreamingIterator<Item = std::result::Result<Page, ParquetError>>,
->(
-    iter: I,
-    metadata: &ColumnChunkMetaData,
-    data_type: DataType,
-) -> Result<Box<dyn Array>> {
-    use DataType::*;
-    Ok(match data_type {
-        FixedSizeBinary(size) => Box::new(fixed_size_binary::iter_to_array(iter, size, metadata)?),
-        other => {
-            return Err(ArrowError::NotYetImplemented(format!(
-                "Can't read {:?} from parquet",
-                other
-            )))
-        }
-    })
-}
-
 pub fn page_iter_to_array<I: StreamingIterator<Item = std::result::Result<Page, ParquetError>>>(
     iter: &mut I,
     metadata: &ColumnChunkMetaData,
     data_type: DataType,
 ) -> Result<Box<dyn Array>> {
-    match metadata.descriptor().base_type() {
-        ParquetType::PrimitiveType {
-            physical_type,
-            converted_type,
-            logical_type,
-            ..
-        } => match (physical_type, converted_type, logical_type) {
-            (PhysicalType::Int32, _, _) => page_iter_i32(iter, metadata, data_type),
-            (PhysicalType::Int64, _, _) => page_iter_i64(iter, metadata, data_type),
-            (PhysicalType::Float, None, None) => {
-                primitive::iter_to_array(iter, metadata, DataType::Float32, |x: f32| x)
-            }
-            (PhysicalType::Double, None, None) => {
-                primitive::iter_to_array(iter, metadata, DataType::Float64, |x: f64| x)
-            }
-            (PhysicalType::Boolean, None, None) => {
-                Ok(Box::new(boolean::iter_to_array(iter, metadata)?))
-            }
-            (PhysicalType::ByteArray, _, _) => page_iter_byte_array(iter, metadata, data_type),
-            (PhysicalType::FixedLenByteArray(_), _, _) => {
-                page_iter_fixed_len_byte_array(iter, metadata, data_type)
-            }
-            (PhysicalType::Int96, _, _) => primitive::iter_to_array(
-                iter,
-                metadata,
-                DataType::Timestamp(TimeUnit::Nanosecond, None),
-                int96_to_i64_ns,
-            ),
-            (p, c, l) => Err(ArrowError::NotYetImplemented(format!(
-                "The conversion of ({:?}, {:?}, {:?}) to arrow still not implemented",
-                p, c, l
-            ))),
+    use DataType::*;
+    match data_type {
+        // INT32
+        UInt8 => primitive::iter_to_array(iter, metadata, data_type, |x: i32| x as u8),
+        UInt16 => primitive::iter_to_array(iter, metadata, data_type, |x: i32| x as u16),
+        UInt32 => primitive::iter_to_array(iter, metadata, data_type, |x: i32| x as u32),
+        Int8 => primitive::iter_to_array(iter, metadata, data_type, |x: i32| x as i8),
+        Int16 => primitive::iter_to_array(iter, metadata, data_type, |x: i32| x as i16),
+        Int32 | Date32 | Time32(_) | Interval(IntervalUnit::YearMonth) => {
+            primitive::iter_to_array(iter, metadata, data_type, |x: i32| x as i32)
+        }
+
+        Timestamp(TimeUnit::Nanosecond, None) => match metadata.descriptor().type_() {
+            ParquetType::PrimitiveType { physical_type, .. } => match physical_type {
+                PhysicalType::Int96 => primitive::iter_to_array(
+                    iter,
+                    metadata,
+                    DataType::Timestamp(TimeUnit::Nanosecond, None),
+                    int96_to_i64_ns,
+                ),
+                _ => primitive::iter_to_array(iter, metadata, data_type, |x: i64| x),
+            },
+            _ => unreachable!(),
         },
-        _ => {
-            // get all primitives in the type_ and their max rep levels
-            match metadata.descriptor().type_() {
-                ParquetType::PrimitiveType {
-                    physical_type,
-                    converted_type,
-                    logical_type,
-                    ..
-                } => match (physical_type, converted_type, logical_type) {
-                    (PhysicalType::Boolean, None, None) => {
-                        boolean::iter_to_array_nested(iter, metadata, data_type)
-                    }
-                    (PhysicalType::Int64, _, _) => page_iter_i64(iter, metadata, data_type),
-                    (PhysicalType::Int32, _, _) => page_iter_i32(iter, metadata, data_type),
-                    (PhysicalType::ByteArray, _, _) => {
-                        page_iter_byte_array(iter, metadata, data_type)
-                    }
-                    _ => todo!(),
+
+        // INT64
+        Int64 | Date64 | Time64(_) | Duration(_) | Timestamp(_, _) => {
+            primitive::iter_to_array(iter, metadata, data_type, |x: i64| x)
+        }
+        UInt64 => primitive::iter_to_array(iter, metadata, data_type, |x: i64| x as u64),
+
+        Float32 => primitive::iter_to_array(iter, metadata, data_type, |x: f32| x),
+        Float64 => primitive::iter_to_array(iter, metadata, data_type, |x: f64| x),
+
+        Boolean => Ok(Box::new(boolean::iter_to_array(iter, metadata)?)),
+
+        Binary | Utf8 => binary::iter_to_array::<i32, _, _>(iter, metadata, &data_type),
+        LargeBinary | LargeUtf8 => binary::iter_to_array::<i64, _, _>(iter, metadata, &data_type),
+        FixedSizeBinary(size) => Ok(Box::new(fixed_size_binary::iter_to_array(
+            iter, size, metadata,
+        )?)),
+
+        List(ref inner) => match inner.data_type() {
+            UInt8 => primitive::iter_to_array_nested(iter, metadata, data_type, |x: i32| x as u8),
+            UInt16 => primitive::iter_to_array_nested(iter, metadata, data_type, |x: i32| x as u16),
+            UInt32 => primitive::iter_to_array_nested(iter, metadata, data_type, |x: i32| x as u32),
+            Int8 => primitive::iter_to_array_nested(iter, metadata, data_type, |x: i32| x as i8),
+            Int16 => primitive::iter_to_array_nested(iter, metadata, data_type, |x: i32| x as i16),
+            Int32 => primitive::iter_to_array_nested(iter, metadata, data_type, |x: i32| x as i32),
+
+            Timestamp(TimeUnit::Nanosecond, None) => match metadata.descriptor().type_() {
+                ParquetType::PrimitiveType { physical_type, .. } => match physical_type {
+                    PhysicalType::Int96 => primitive::iter_to_array_nested(
+                        iter,
+                        metadata,
+                        DataType::Timestamp(TimeUnit::Nanosecond, None),
+                        int96_to_i64_ns,
+                    ),
+                    _ => primitive::iter_to_array(iter, metadata, data_type, |x: i64| x),
                 },
                 _ => unreachable!(),
+            },
+
+            // INT64
+            Int64 | Date64 | Time64(_) | Duration(_) | Timestamp(_, _) => {
+                primitive::iter_to_array_nested(iter, metadata, data_type, |x: i64| x)
             }
-        }
+            UInt64 => primitive::iter_to_array_nested(iter, metadata, data_type, |x: i64| x as u64),
+
+            Float32 => primitive::iter_to_array_nested(iter, metadata, data_type, |x: f32| x),
+            Float64 => primitive::iter_to_array_nested(iter, metadata, data_type, |x: f64| x),
+
+            Boolean => boolean::iter_to_array_nested(iter, metadata, data_type),
+
+            Binary | Utf8 => binary::iter_to_array_nested::<i32, _, _>(iter, metadata, data_type),
+            LargeBinary | LargeUtf8 => {
+                binary::iter_to_array_nested::<i64, _, _>(iter, metadata, data_type)
+            }
+            other => Err(ArrowError::NotYetImplemented(format!(
+                "The conversion of {:?} to arrow still not implemented",
+                other
+            ))),
+        },
+        other => Err(ArrowError::NotYetImplemented(format!(
+            "The conversion of {:?} to arrow still not implemented",
+            other
+        ))),
     }
 }
 
