0db69bdae303feb4f337c86e16da222b4262c47f
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2021-08-11 05:17:16 +0000

    deploy: 4b987fcd392d478e9f80da9c32356760b782da09

--- docs/implementors/core/ops/deref/trait.DerefMut.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["arrow2"] = [{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"arrow2/types/trait.NativeType.html\" title=\"trait arrow2::types::NativeType\">NativeType</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"arrow2/buffer/struct.MutableBuffer.html\" title=\"struct arrow2::buffer::MutableBuffer\">MutableBuffer</a>&lt;T&gt;","synthetic":false,"types":["arrow2::buffer::mutable::MutableBuffer"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
