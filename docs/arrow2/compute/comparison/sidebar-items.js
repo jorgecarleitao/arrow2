cf00d9cc22f4e793abb5c9d122fbed8c6e04fd12
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2021-08-12 06:38:51 +0000

    deploy: c7e1a4d6f4e3012543c5888e669ccb09d786f18f

--- docs/arrow2/compute/comparison/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"enum":[["Operator",""]],"fn":[["boolean_compare_scalar",""],["can_compare","Checks if an array of type `datatype` can be compared with another array of the same type."],["compare",""],["primitive_compare_scalar",""],["utf8_compare_scalar",""]]});
