f9f85ff34f2f5c6e77a1a25a4c6a147cf4e0e01b
Jorge C. Leitao <jorgecarleitao@gmail.com>
2021-08-15 06:56:48 +0000

    Moved tests.

--- tests/it/array/mod.rs
+++ tests/it/array/mod.rs
@@ -8,4 +8,53 @@ mod growable;
 mod list;
 mod ord;
 mod primitive;
+mod union;
 mod utf8;
+
+use arrow2::array::{clone, new_empty_array, new_null_array};
+use arrow2::datatypes::{DataType, Field};
+
+#[test]
+fn nulls() {
+    let datatypes = vec![
+        DataType::Int32,
+        DataType::Float64,
+        DataType::Utf8,
+        DataType::Binary,
+        DataType::List(Box::new(Field::new("a", DataType::Binary, true))),
+    ];
+    let a = datatypes
+        .into_iter()
+        .all(|x| new_null_array(x, 10).null_count() == 10);
+    assert!(a);
+}
+
+#[test]
+fn empty() {
+    let datatypes = vec![
+        DataType::Int32,
+        DataType::Float64,
+        DataType::Utf8,
+        DataType::Binary,
+        DataType::List(Box::new(Field::new("a", DataType::Binary, true))),
+        DataType::Union(vec![Field::new("a", DataType::Binary, true)], None, true),
+        DataType::Union(vec![Field::new("a", DataType::Binary, true)], None, false),
+    ];
+    let a = datatypes.into_iter().all(|x| new_empty_array(x).len() == 0);
+    assert!(a);
+}
+
+#[test]
+fn test_clone() {
+    let datatypes = vec![
+        DataType::Int32,
+        DataType::Float64,
+        DataType::Utf8,
+        DataType::Binary,
+        DataType::List(Box::new(Field::new("a", DataType::Binary, true))),
+    ];
+    let a = datatypes
+        .into_iter()
+        .all(|x| clone(new_null_array(x.clone(), 10).as_ref()) == new_null_array(x, 10));
+    assert!(a);
+}
---
+++ tests/it/array/union.rs
@@ -0,0 +1,23 @@
+use std::sync::Arc;
+
+use arrow2::{array::*, buffer::Buffer, datatypes::*, error::Result};
+
+#[test]
+fn display() -> Result<()> {
+    let fields = vec![
+        Field::new("a", DataType::Int32, true),
+        Field::new("b", DataType::Utf8, true),
+    ];
+    let data_type = DataType::Union(fields, None, true);
+    let types = Buffer::from(&[0, 0, 1]);
+    let fields = vec![
+        Arc::new(Int32Array::from(&[Some(1), None, Some(2)])) as Arc<dyn Array>,
+        Arc::new(Utf8Array::<i32>::from(&[Some("a"), Some("b"), Some("c")])) as Arc<dyn Array>,
+    ];
+
+    let array = UnionArray::from_data(data_type, types, fields, None);
+
+    assert_eq!(format!("{}", array), "UnionArray[1, , c]");
+
+    Ok(())
+}
