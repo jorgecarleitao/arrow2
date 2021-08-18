b83ab4d9bb2d5645bdf2a3670296cf287ced793a
Jorge Leitao <jorgecarleitao@gmail.com>
2021-08-18 01:35:50 +0100

    Added more tests to MutablePrimitiveArray (#294)

--- tests/it/array/primitive/mutable.rs
+++ tests/it/array/primitive/mutable.rs
@@ -2,19 +2,60 @@ use arrow2::{
     array::*,
     bitmap::{Bitmap, MutableBitmap},
     buffer::MutableBuffer,
+    datatypes::DataType,
 };
 use std::iter::FromIterator;
+
+#[test]
+fn from_and_into_data() {
+    let a = MutablePrimitiveArray::from_data(
+        DataType::Int32,
+        MutableBuffer::from([1i32, 0]),
+        Some(MutableBitmap::from([true, false])),
+    );
+    assert_eq!(a.len(), 2);
+    let (a, b, c) = a.into_data();
+    assert_eq!(a, DataType::Int32);
+    assert_eq!(b, MutableBuffer::from([1i32, 0]));
+    assert_eq!(c, Some(MutableBitmap::from([true, false])));
+}
+
+#[test]
+fn to() {
+    let a = MutablePrimitiveArray::from_data(
+        DataType::Int32,
+        MutableBuffer::from([1i32, 0]),
+        Some(MutableBitmap::from([true, false])),
+    );
+    let a = a.to(DataType::Date32);
+    assert_eq!(a.data_type(), &DataType::Date32);
+}
+
+#[test]
+fn values_mut_slice() {
+    let mut a = MutablePrimitiveArray::from_data(
+        DataType::Int32,
+        MutableBuffer::from([1i32, 0]),
+        Some(MutableBitmap::from([true, false])),
+    );
+    let values = a.values_mut_slice();
+
+    values[0] = 10;
+    assert_eq!(a.values()[0], 10);
+}
 
 #[test]
 fn push() {
     let mut a = MutablePrimitiveArray::<i32>::new();
     a.push(Some(1));
     a.push(None);
-    assert_eq!(a.len(), 2);
+    a.push_null();
+    assert_eq!(a.len(), 3);
     assert!(a.is_valid(0));
     assert!(!a.is_valid(1));
+    assert!(!a.is_valid(2));
 
-    assert_eq!(a.values(), &MutableBuffer::from([1, 0]));
+    assert_eq!(a.values(), &MutableBuffer::from([1, 0, 0]));
 }
 
 #[test]
@@ -29,6 +70,17 @@ fn set() {
     assert!(a.is_valid(1));
 
     assert_eq!(a.values(), &MutableBuffer::from([2, 1]));
+
+    let mut a = MutablePrimitiveArray::<i32>::from_slice([1, 2]);
+
+    a.set(0, Some(2));
+    a.set(1, None);
+
+    assert_eq!(a.len(), 2);
+    assert!(a.is_valid(0));
+    assert!(!a.is_valid(1));
+
+    assert_eq!(a.values(), &MutableBuffer::from([2, 0]));
 }
 
 #[test]
@@ -71,4 +123,20 @@ fn extend_trusted_len() {
         &Some(MutableBitmap::from([true, true, false, true]))
     );
     assert_eq!(a.values(), &MutableBuffer::<i32>::from([1, 2, 0, 4]));
+}
+
+#[test]
+fn set_validity() {
+    let mut a = MutablePrimitiveArray::<i32>::new();
+    a.extend_trusted_len(vec![Some(1), Some(2)].into_iter());
+    assert_eq!(a.validity(), &None);
+    a.set_validity(Some(MutableBitmap::from([false, true])));
+    assert_eq!(a.validity(), &Some(MutableBitmap::from([false, true])));
+}
+
+#[test]
+fn set_values() {
+    let mut a = MutablePrimitiveArray::<i32>::from_slice([1, 2]);
+    a.set_values(MutableBuffer::from([1, 3]));
+    assert_eq!(a.values().as_slice(), [1, 3]);
 }
