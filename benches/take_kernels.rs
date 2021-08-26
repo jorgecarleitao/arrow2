c6320f2f9b30be164ecae83d18e91d4395057a2f
Jorge C. Leitao <jorgecarleitao@gmail.com>
2021-08-26 19:19:50 +0000

    Improved benches for take

--- benches/take_kernels.rs
+++ benches/take_kernels.rs
@@ -1,25 +1,6 @@
-// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
-
-#[macro_use]
-extern crate criterion;
-use criterion::Criterion;
-
 use rand::{rngs::StdRng, Rng, SeedableRng};
+
+use criterion::{criterion_group, criterion_main, Criterion};
 
 use arrow2::compute::take;
 use arrow2::util::bench_util::*;
@@ -49,55 +30,43 @@ fn bench_take(values: &dyn Array, indices: &PrimitiveArray<i32>) {
 }
 
 fn add_benchmark(c: &mut Criterion) {
-    let values = create_primitive_array::<i32>(512, DataType::Int32, 0.0);
-    let indices = create_random_index(512, 0.0);
-    c.bench_function("take i32 512", |b| b.iter(|| bench_take(&values, &indices)));
-    let values = create_primitive_array::<i32>(1024, DataType::Int32, 0.0);
-    let indices = create_random_index(1024, 0.0);
-    c.bench_function("take i32 1024", |b| {
-        b.iter(|| bench_take(&values, &indices))
-    });
+    (10..=20).step_by(2).for_each(|log2_size| {
+        let size = 2usize.pow(log2_size);
 
-    let indices = create_random_index(512, 0.5);
-    c.bench_function("take i32 nulls 512", |b| {
-        b.iter(|| bench_take(&values, &indices))
-    });
-    let values = create_primitive_array::<i32>(1024, DataType::Int32, 0.0);
-    let indices = create_random_index(1024, 0.5);
-    c.bench_function("take i32 nulls 1024", |b| {
-        b.iter(|| bench_take(&values, &indices))
-    });
+        let values = create_primitive_array::<i32>(size, DataType::Int32, 0.0);
+        let values_nulls = create_primitive_array::<i32>(size, DataType::Int32, 0.2);
+        let indices = create_random_index(size, 0.0);
+        let indices_nulls = create_random_index(size, 0.5);
+        c.bench_function(&format!("take i32 2^{}", log2_size), |b| {
+            b.iter(|| bench_take(&values, &indices))
+        });
 
-    let values = create_boolean_array(512, 0.0, 0.5);
-    let indices = create_random_index(512, 0.0);
-    c.bench_function("take bool 512", |b| {
-        b.iter(|| bench_take(&values, &indices))
-    });
-    let values = create_boolean_array(1024, 0.0, 0.5);
-    let indices = create_random_index(1024, 0.0);
-    c.bench_function("take bool 1024", |b| {
-        b.iter(|| bench_take(&values, &indices))
-    });
+        c.bench_function(&format!("take i32 nulls 2^{}", log2_size), |b| {
+            b.iter(|| bench_take(&values, &indices_nulls))
+        });
 
-    let values = create_boolean_array(512, 0.0, 0.5);
-    let indices = create_random_index(512, 0.5);
-    c.bench_function("take bool nulls 512", |b| {
-        b.iter(|| bench_take(&values, &indices))
-    });
-    let values = create_boolean_array(1024, 0.0, 0.5);
-    let indices = create_random_index(1024, 0.5);
-    c.bench_function("take bool nulls 1024", |b| {
-        b.iter(|| bench_take(&values, &indices))
-    });
+        c.bench_function(&format!("take i32 values nulls 2^{}", log2_size), |b| {
+            b.iter(|| bench_take(&values_nulls, &indices))
+        });
 
-    let values = create_string_array::<i32>(512, 0.0, 42);
-    let indices = create_random_index(512, 0.0);
-    c.bench_function("take str 512", |b| b.iter(|| bench_take(&values, &indices)));
+        let values = create_boolean_array(size, 0.0, 0.5);
+        c.bench_function(&format!("take bool 2^{}", log2_size), |b| {
+            b.iter(|| bench_take(&values, &indices))
+        });
 
-    let values = create_string_array::<i32>(1024, 0.0, 42);
-    let indices = create_random_index(1024, 0.0);
-    c.bench_function("take str 1024", |b| {
-        b.iter(|| bench_take(&values, &indices))
+        c.bench_function(&format!("take bool nulls 2^{}", log2_size), |b| {
+            b.iter(|| bench_take(&values, &indices_nulls))
+        });
+
+        let values = create_string_array::<i32>(512, 0.0, 42);
+        c.bench_function(&format!("take str 2^{}", log2_size), |b| {
+            b.iter(|| bench_take(&values, &indices))
+        });
+
+        let values = create_string_array::<i32>(512, 0.0, 42);
+        c.bench_function(&format!("take str nulls 2^{}", log2_size), |b| {
+            b.iter(|| bench_take(&values, &indices_nulls))
+        });
     });
 
     let values = create_string_array::<i32>(512, 0.0, 42);
