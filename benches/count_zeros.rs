d4b6aaa0985d44b9fc12a12524aa3954a8ce1537
Jorge C. Leitao <jorgecarleitao@gmail.com>
2021-08-26 10:28:57 +0000

    Refactored benches for null_count.

---
+++ benches/count_zeros.rs
@@ -0,0 +1,24 @@
+use arrow2::bitmap::utils::null_count;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
+fn add_benchmark(c: &mut Criterion) {
+    (10..=20).step_by(2).for_each(|log2_size| {
+        let size = 2usize.pow(log2_size);
+
+        let bytes = (0..size as u32)
+            .map(|x| 0b01011011u8.rotate_left(x))
+            .collect::<Vec<_>>();
+
+        c.bench_function(&format!("count_zeros 2^{}", log2_size), |b| {
+            b.iter(|| null_count(&bytes, 0, bytes.len() * 8))
+        });
+
+        c.bench_function(&format!("count_zeros offset 2^{}", log2_size), |b| {
+            b.iter(|| null_count(&bytes, 10, bytes.len() * 8 - 10))
+        });
+    })
+}
+
+criterion_group!(benches, add_benchmark);
+criterion_main!(benches);
