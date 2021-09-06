77e153da59936c59e6dea81e34ed3de1468c2c95
Jorge C. Leitao <jorgecarleitao@gmail.com>
2021-09-06 20:03:29 +0000

    Added bench of arithmetics.

--- benches/arithmetic_kernels.rs
+++ benches/arithmetic_kernels.rs
@@ -1,20 +1,3 @@
-// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
-
 #[macro_use]
 extern crate criterion;
 use criterion::Criterion;
@@ -22,10 +5,11 @@ use criterion::Criterion;
 use arrow2::array::*;
 use arrow2::util::bench_util::*;
 use arrow2::{
-    compute::arithmetics::basic::div::div_scalar, datatypes::DataType, types::NativeType,
+    compute::arithmetics::basic::add::add, compute::arithmetics::basic::div::div_scalar,
+    datatypes::DataType, types::NativeType,
 };
 use num_traits::NumCast;
-use std::ops::Div;
+use std::ops::{Add, Div};
 
 fn bench_div_scalar<T>(lhs: &PrimitiveArray<T>, rhs: &T)
 where
@@ -34,17 +18,31 @@ where
     criterion::black_box(div_scalar(lhs, rhs));
 }
 
-fn add_benchmark(c: &mut Criterion) {
-    let size = 65536;
-    let arr = create_primitive_array_with_seed::<u64>(size, DataType::UInt64, 0.0, 43);
+fn bench_add<T>(lhs: &PrimitiveArray<T>, rhs: &PrimitiveArray<T>)
+where
+    T: NativeType + Add<Output = T> + NumCast,
+{
+    criterion::black_box(add(lhs, rhs)).unwrap();
+}
 
-    c.bench_function("divide_scalar 4", |b| {
-        // 4 is a very fast optimizable divisor
-        b.iter(|| bench_div_scalar(&arr, &4))
-    });
-    c.bench_function("divide_scalar prime", |b| {
-        // large prime number that is probably harder to simplify
-        b.iter(|| bench_div_scalar(&arr, &524287))
+fn add_benchmark(c: &mut Criterion) {
+    (10..=20).step_by(2).for_each(|log2_size| {
+        let size = 2usize.pow(log2_size);
+        let arr_a = create_primitive_array_with_seed::<u64>(size, DataType::UInt64, 0.0, 43);
+        let arr_b = create_primitive_array_with_seed::<u64>(size, DataType::UInt64, 0.0, 42);
+
+        c.bench_function(&format!("divide_scalar 2^{}", log2_size), |b| {
+            // 4 is a very fast optimizable divisor
+            b.iter(|| bench_div_scalar(&arr_a, &4))
+        });
+        c.bench_function(&format!("divide_scalar prime 2^{}", log2_size), |b| {
+            // large prime number that is probably harder to simplify
+            b.iter(|| bench_div_scalar(&arr_a, &524287))
+        });
+
+        c.bench_function(&format!("add 2^{}", log2_size), |b| {
+            b.iter(|| bench_add(&arr_a, &arr_b))
+        });
     });
 }
 
