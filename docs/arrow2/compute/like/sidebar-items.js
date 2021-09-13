5ff7b8ed5a8717cfb751756531e86f84667101c6
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2021-09-13 15:10:38 +0000

    deploy: 227ab3b6c5cacc8b9bbc3628b678941ef2f6d808

---
+++ docs/arrow2/compute/like/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["like_binary","Returns `lhs LIKE rhs` operation on two [`BinaryArray`]."],["like_binary_scalar","Returns `lhs LIKE rhs` operation."],["like_utf8","Returns `lhs LIKE rhs` operation on two [`Utf8Array`]."],["like_utf8_scalar","Returns `lhs LIKE rhs` operation."],["nlike_binary",""],["nlike_binary_scalar",""],["nlike_utf8",""],["nlike_utf8_scalar",""]]});
