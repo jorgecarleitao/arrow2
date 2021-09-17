0db09e82e1539d1f29fd83531509a30c049cec27
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2021-09-17 22:43:58 +0000

    deploy: 4701f1041b55ed12f909780b0b863c9271a4a89c

--- docs/implementors/core/ops/bit/trait.Not.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["arrow2"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for &amp;<a class=\"struct\" href=\"arrow2/bitmap/struct.Bitmap.html\" title=\"struct arrow2::bitmap::Bitmap\">Bitmap</a>","synthetic":false,"types":["arrow2::bitmap::immutable::Bitmap"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
