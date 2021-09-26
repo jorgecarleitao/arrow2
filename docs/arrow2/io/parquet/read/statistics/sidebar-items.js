40da7431892db0e85ddb3b11114b93096a2b479a
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2021-09-26 15:31:30 +0000

    deploy: 90887c25d9e6159cde94f67ded2f7be308c7f84d

---
+++ docs/arrow2/io/parquet/read/statistics/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["deserialize_statistics",""]],"struct":[["BinaryStatistics",""],["BooleanStatistics",""],["PrimitiveStatistics",""],["Utf8Statistics",""]],"trait":[["Statistics","Trait denoting a deserialized parquet statistics (into arrow)."]]});
