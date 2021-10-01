8a90ecec790044c6f9713ce62b35c87e7101566f
Jorge C. Leitao <jorgecarleitao@gmail.com>
2021-10-01 11:40:09 +0000

    Added support for timestamps with timezone to CSV.

--- src/io/csv/read/mod.rs
+++ src/io/csv/read/mod.rs
@@ -8,5 +8,5 @@ pub use csv::{ByteRecord, Reader, ReaderBuilder};
 mod infer_schema;
 
 pub use deserialize::{deserialize_batch, deserialize_column};
-pub use infer_schema::infer_schema;
+pub use infer_schema::{infer, infer_schema};
 pub use reader::*;
