b768aa6082e18163d5351704b976c13aa2db4b31
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2021-11-01 19:45:40 +0000

    deploy: e4c37f928245a8932e7b56f02bbedcf2e79c6622

---
+++ docs/arrow2/compute/partition/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["lexicographical_partition_ranges","Given a list of already sorted columns, find partition ranges that would partition lexicographically equal values across columns."]]});
