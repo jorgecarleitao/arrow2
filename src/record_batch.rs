e55b4509896c8b42d35fd132c9ac25e56c6cad4d
Jorge C. Leitao <jorgecarleitao@gmail.com>
2021-11-23 07:24:38 +0000

    Removed un-needed trait.

--- src/record_batch.rs
+++ src/record_batch.rs
@@ -332,21 +332,3 @@ impl From<RecordBatch> for StructArray {
         StructArray::from_data(DataType::Struct(fields), values, None)
     }
 }
-
-/// Trait for types that can read `RecordBatch`'s.
-pub trait RecordBatchReader: Iterator<Item = Result<RecordBatch>> {
-    /// Returns the schema of this `RecordBatchReader`.
-    ///
-    /// Implementation of this trait should guarantee that all `RecordBatch`'s returned by this
-    /// reader should have the same schema as returned from this method.
-    fn schema(&self) -> &Schema;
-
-    /// Reads the next `RecordBatch`.
-    #[deprecated(
-        since = "2.0.0",
-        note = "This method is deprecated in favour of `next` from the trait Iterator."
-    )]
-    fn next_batch(&mut self) -> Result<Option<RecordBatch>> {
-        self.next().transpose()
-    }
-}
