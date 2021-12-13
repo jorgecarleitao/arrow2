77a8d1737e96a598bae5673b637f5c796e1f2342
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2021-12-13 20:05:18 +0000

    deploy: 134a57f3b592b56c6897ac46a62099d2b8a17ac4

--- docs/arrow2/io/ipc/write/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"enum":[["Compression","Compression codec"]],"fn":[["schema_to_bytes","Converts"],["write",""],["write_dictionary",""]],"mod":[["stream_async","`async` writing of arrow streams"]],"struct":[["FileWriter","Arrow file writer"],["StreamWriter","Arrow stream writer"],["WriteOptions","Options declaring the behaviour of writing to IPC"]]});
