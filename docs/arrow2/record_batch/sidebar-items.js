424c4d38007b85f1e56e92199971caca2648677f
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2021-12-20 19:34:41 +0000

    deploy: 72c875cd80315ea3f62c21434414a592f828bafb

--- docs/arrow2/record_batch/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["RecordBatch","A two-dimensional dataset with a number of columns ([`Array`]) and rows and defined `Schema`."],["RecordBatchOptions","Options that control the behaviour used when creating a [`RecordBatch`]."]]});
