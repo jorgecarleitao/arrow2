78faa366c4598111bb8b2a9693bc94ef79397263
Jorge C. Leitao <jorgecarleitao@gmail.com>
2021-12-27 06:44:23 +0000

    Simplified reading

--- src/io/ipc/read/schema.rs
+++ src/io/ipc/read/schema.rs
@@ -32,13 +32,17 @@ fn deserialize_field(ipc_field: ipc::Field) -> (Field, IpcField) {
 
 fn read_metadata(field: &ipc::Field) -> Metadata {
     if let Some(list) = field.custom_metadata() {
-        let mut metadata_map = BTreeMap::default();
-        for kv in list {
-            if let (Some(k), Some(v)) = (kv.key(), kv.value()) {
-                metadata_map.insert(k.to_string(), v.to_string());
+        if !list.is_empty() {
+            let mut metadata_map = BTreeMap::default();
+            for kv in list {
+                if let (Some(k), Some(v)) = (kv.key(), kv.value()) {
+                    metadata_map.insert(k.to_string(), v.to_string());
+                }
             }
+            Some(metadata_map)
+        } else {
+            None
         }
-        Some(metadata_map)
     } else {
         None
     }
