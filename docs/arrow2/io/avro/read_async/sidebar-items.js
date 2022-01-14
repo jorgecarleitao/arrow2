9e6ae1e46634bc145910cb9461ac00eeded89455
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2022-01-14 21:47:27 +0000

    deploy: 289d38a00d7ff0f61de366bde2bd84c4be06a913

--- docs/arrow2/io/avro/read_async/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"fn":[["block_stream","Returns a fallible [`Stream`] of Avro blocks bound to `reader`"],["read_metadata","Reads the avro metadata from `reader` into a [`AvroSchema`], [`Compression`] and magic marker."]]});
