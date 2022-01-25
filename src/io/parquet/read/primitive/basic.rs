f772eb2a1db5629131d8c5592ab026a494253fc7
Jorge C. Leitao <jorgecarleitao@gmail.com>
2022-01-25 18:57:20 +0000

    Primitive

--- src/io/parquet/read/primitive/basic.rs
+++ src/io/parquet/read/primitive/basic.rs
@@ -38,7 +38,9 @@ where
     let indices_buffer = &indices_buffer[1..];
 
     let indices = hybrid_rle::HybridRleDecoder::new(indices_buffer, bit_width as u32, additional);
-    indices.map(move |index| op(dict_values[index as usize]))
+    indices
+        .map(move |index| dict_values[index as usize])
+        .map(op)
 }
 
 fn read_dict_buffer_optional<T, A, F>(
@@ -191,7 +193,7 @@ where
 }
 
 #[derive(Debug)]
-struct RequiredPrimitiveDataPage<'a, T, P, G, F>
+struct RequiredPage<'a, T, P, G, F>
 where
     T: NativeType,
     P: ParquetNativeType,
@@ -202,7 +204,7 @@ where
     phantom: std::marker::PhantomData<P>,
 }
 
-impl<'a, T, P, G, F> RequiredPrimitiveDataPage<'a, T, P, G, F>
+impl<'a, T, P, G, F> RequiredPage<'a, T, P, G, F>
 where
     T: NativeType,
     P: ParquetNativeType,
@@ -226,7 +228,7 @@ where
 }
 
 #[derive(Debug)]
-struct OptionalPrimitiveDataPage<'a, T, P, G, F>
+struct OptionalPage<'a, T, P, G, F>
 where
     T: NativeType,
     P: ParquetNativeType,
@@ -240,7 +242,7 @@ where
     length: usize,
 }
 
-impl<'a, T, P, G, F> OptionalPrimitiveDataPage<'a, T, P, G, F>
+impl<'a, T, P, G, F> OptionalPage<'a, T, P, G, F>
 where
     T: NativeType,
     P: ParquetNativeType,
@@ -265,34 +267,133 @@ where
     }
 }
 
+#[inline]
+fn values_iter1<P, T, G, F>(
+    indices_buffer: &[u8],
+    additional: usize,
+    op1: G,
+    op2: F,
+) -> std::iter::Map<std::iter::Map<hybrid_rle::HybridRleDecoder, G>, F>
+where
+    P: ParquetNativeType,
+    T: NativeType,
+    G: Fn(u32) -> P,
+    F: Fn(P) -> T,
+{
+    // SPEC: Data page format: the bit width used to encode the entry ids stored as 1 byte (max bit width = 32),
+    // SPEC: followed by the values encoded using RLE/Bit packed described above (with the given bit width).
+    let bit_width = indices_buffer[0];
+    let indices_buffer = &indices_buffer[1..];
+
+    let indices = hybrid_rle::HybridRleDecoder::new(indices_buffer, bit_width as u32, additional);
+    indices.map(op1).map(op2)
+}
+
+#[derive(Debug)]
+struct RequiredDictionaryPage<'a, T, P, F>
+where
+    T: NativeType,
+    P: ParquetNativeType,
+    F: Fn(P) -> T,
+{
+    values: std::iter::Map<
+        std::iter::Map<hybrid_rle::HybridRleDecoder<'a>, Box<dyn Fn(u32) -> P + 'a>>,
+        F,
+    >,
+    phantom: std::marker::PhantomData<P>,
+}
+
+impl<'a, T, P, F> RequiredDictionaryPage<'a, T, P, F>
+where
+    T: NativeType,
+    P: ParquetNativeType,
+    F: Fn(P) -> T,
+{
+    fn new(page: &'a DataPage, dict: &'a PrimitivePageDict<P>, op2: F) -> Self {
+        let values = dict.values();
+        let op1 = Box::new(move |index: u32| values[index as usize]) as Box<dyn Fn(u32) -> P>;
+
+        let values = values_iter1(page.buffer(), page.num_values(), op1, op2);
+
+        Self {
+            phantom: Default::default(),
+            values,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct OptionalDictionaryPage<'a, T, P, F>
+where
+    T: NativeType,
+    P: ParquetNativeType,
+    F: Fn(P) -> T,
+{
+    validity: Convert<hybrid_rle::Decoder<'a>>,
+    values: std::iter::Map<
+        std::iter::Map<hybrid_rle::HybridRleDecoder<'a>, Box<dyn Fn(u32) -> P + 'a>>,
+        F,
+    >,
+    phantom: std::marker::PhantomData<P>,
+    offset: usize,
+    length: usize,
+}
+
+impl<'a, T, P, F> OptionalDictionaryPage<'a, T, P, F>
+where
+    T: NativeType,
+    P: ParquetNativeType,
+    F: Fn(P) -> T,
+{
+    fn new(page: &'a DataPage, dict: &'a PrimitivePageDict<P>, op2: F) -> Self {
+        let (_, validity_buffer, values_buffer, _) =
+            other_utils::split_buffer(page, page.descriptor());
+
+        let values = dict.values();
+        let op1 = Box::new(move |index: u32| values[index as usize]) as Box<dyn Fn(u32) -> P>;
+
+        let validity = convert(hybrid_rle::Decoder::new(validity_buffer, 1));
+
+        let values = values_iter1(values_buffer, page.num_values(), op1, op2);
+
+        Self {
+            phantom: Default::default(),
+            values,
+            validity,
+            offset: 0,
+            length: page.num_values(),
+        }
+    }
+}
+
 // The state of a `DataPage` of `Primitive` parquet primitive type
 #[derive(Debug)]
 enum PrimitivePageState<'a, T, P, G, F>
 where
     T: NativeType,
     P: ParquetNativeType,
-    G: for<'b> Fn(&'b [u8]) -> P,
-    F: Fn(P) -> T,
+    G: Copy + for<'b> Fn(&'b [u8]) -> P,
+    F: Copy + Fn(P) -> T,
 {
-    Optional(OptionalPrimitiveDataPage<'a, T, P, G, F>),
-    Required(RequiredPrimitiveDataPage<'a, T, P, G, F>),
-    RequiredDictionary(RequiredPrimitiveDataPage<'a, T, P, G, F>),
-    OptionalDictionary(RequiredPrimitiveDataPage<'a, T, P, G, F>),
+    Optional(OptionalPage<'a, T, P, G, F>),
+    Required(RequiredPage<'a, T, P, G, F>),
+    RequiredDictionary(RequiredDictionaryPage<'a, T, P, F>),
+    OptionalDictionary(OptionalDictionaryPage<'a, T, P, F>),
 }
 
 impl<'a, T, P, G, F> other_utils::PageState<'a> for PrimitivePageState<'a, T, P, G, F>
 where
     T: NativeType,
     P: ParquetNativeType,
-    G: for<'b> Fn(&'b [u8]) -> P,
-    F: Fn(P) -> T,
+    G: Copy + for<'b> Fn(&'b [u8]) -> P,
+    F: Copy + Fn(P) -> T,
 {
     fn len(&self) -> usize {
         match self {
             PrimitivePageState::Optional(optional) => optional.length - optional.offset,
             PrimitivePageState::Required(required) => required.values.size_hint().0,
-            PrimitivePageState::RequiredDictionary(_) => todo!(),
-            PrimitivePageState::OptionalDictionary(_) => todo!(),
+            PrimitivePageState::RequiredDictionary(required) => required.values.size_hint().0,
+            PrimitivePageState::OptionalDictionary(optional) => optional.length - optional.offset,
         }
     }
 }
@@ -309,19 +410,31 @@ where
     G: Copy + for<'b> Fn(&'b [u8]) -> P,
     F: Copy + Fn(P) -> T,
 {
-    match (page.encoding(), is_optional) {
-        (Encoding::Plain, true) => Ok(PrimitivePageState::Optional(
-            OptionalPrimitiveDataPage::new(page, op1, op2),
-        )),
-        (Encoding::Plain, false) => Ok(PrimitivePageState::Required(
-            RequiredPrimitiveDataPage::new(page, op1, op2),
-        )),
+    match (page.encoding(), page.dictionary_page(), is_optional) {
+        (Encoding::PlainDictionary | Encoding::RleDictionary, Some(dict), false) => {
+            let dict = dict.as_any().downcast_ref().unwrap();
+            Ok(PrimitivePageState::RequiredDictionary(
+                RequiredDictionaryPage::new(page, dict, op2),
+            ))
+        }
+        (Encoding::PlainDictionary | Encoding::RleDictionary, Some(dict), true) => {
+            let dict = dict.as_any().downcast_ref().unwrap();
+            Ok(PrimitivePageState::OptionalDictionary(
+                OptionalDictionaryPage::new(page, dict, op2),
+            ))
+        }
+        (Encoding::Plain, None, true) => Ok(PrimitivePageState::Optional(OptionalPage::new(
+            page, op1, op2,
+        ))),
+        (Encoding::Plain, None, false) => Ok(PrimitivePageState::Required(RequiredPage::new(
+            page, op1, op2,
+        ))),
         _ => Err(other_utils::not_implemented(
             &page.encoding(),
             is_optional,
             false,
             "any",
-            "Boolean",
+            "Primitive",
         )),
     }
 }
@@ -344,8 +457,8 @@ impl<'a, T, P, G, F> other_utils::Decoder<'a, T, Vec<T>> for PrimitiveDecoder<T,
 where
     T: NativeType,
     P: ParquetNativeType,
-    G: for<'b> Fn(&'b [u8]) -> P,
-    F: Fn(P) -> T,
+    G: Copy + for<'b> Fn(&'b [u8]) -> P,
+    F: Copy + Fn(P) -> T,
 {
     type State = PrimitivePageState<'a, T, P, G, F>;
     type Array = PrimitiveArray<T>;
@@ -369,11 +482,26 @@ where
             PrimitivePageState::Required(page) => {
                 values.extend(page.values.by_ref().take(remaining));
             }
-            _ => todo!(),
+            PrimitivePageState::OptionalDictionary(page) => extend_from_decoder(
+                validity,
+                &mut page.validity,
+                page.length,
+                &mut page.offset,
+                Some(remaining),
+                values,
+                &mut page.values,
+            ),
+            PrimitivePageState::RequiredDictionary(page) => {
+                values.extend(page.values.by_ref().take(remaining));
+            }
         }
     }
 
     fn finish(data_type: DataType, values: Vec<T>, validity: MutableBitmap) -> Self::Array {
+        let data_type = match data_type {
+            DataType::Dictionary(_, values, _) => values.as_ref().clone(),
+            _ => data_type,
+        };
         PrimitiveArray::from_data(data_type, values.into(), validity.into())
     }
 }
@@ -404,8 +532,8 @@ where
     T: NativeType,
 
     P: ParquetNativeType,
-    G: for<'b> Fn(&'b [u8]) -> P,
-    F: Fn(P) -> T,
+    G: Copy + for<'b> Fn(&'b [u8]) -> P,
+    F: Copy + Fn(P) -> T,
 {
     pub fn new(
         iter: I,
@@ -453,21 +581,22 @@ where
             (_, Err(e)) => Some(Err(e.into())),
             (None, Ok(None)) => None,
             (state, Ok(Some(page))) => {
-                // there is a new page => consume the page from the start
-                let maybe_page = build_state(page, self.is_optional, self.op1, self.op2);
-                let page = match maybe_page {
-                    Ok(page) => page,
-                    Err(e) => return Some(Err(e)),
-                };
+                let maybe_array = {
+                    // there is a new page => consume the page from the start
+                    let maybe_page = build_state(page, self.is_optional, self.op1, self.op2);
+                    let page = match maybe_page {
+                        Ok(page) => page,
+                        Err(e) => return Some(Err(e)),
+                    };
 
-                let maybe_array =
                     other_utils::extend_from_new_page::<PrimitiveDecoder<T, P, G, F>, _, _>(
                         page,
                         state,
                         &self.data_type,
                         self.chunk_size,
                         &mut self.items,
-                    );
+                    )
+                };
                 match maybe_array {
                     Ok(Some(array)) => Some(Ok(array)),
                     Ok(None) => self.next(),
