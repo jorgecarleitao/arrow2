df7d5867cd351e9c555c078d45cc019cab93224e
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2022-02-05 20:08:31 +0000

    deploy: 8330eea73f92a36498984df7f4c8de5ccf8811a2

---
+++ docs/arrow2/buffer/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Buffer","[`Buffer`] is a contiguous memory region that can be shared across thread boundaries. The easiest way to think about `Buffer<T>` is being equivalent to an immutable `Vec<T>`, with the following differences:"]]});
