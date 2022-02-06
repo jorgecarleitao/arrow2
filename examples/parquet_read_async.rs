3d528c99589e96f0539de4c07b11843fa22f23ac
Jorge Leitao <jorgecarleitao@gmail.com>
2022-02-06 12:56:38 +0100

    Improved flexibility of reading parquet (#820)

--- examples/parquet_read_async.rs
+++ examples/parquet_read_async.rs
@@ -39,7 +39,7 @@ async fn main() -> Result<()> {
         // A row group is consumed in two steps: the first step is to read the (compressed)
         // columns into memory, which is IO-bounded.
         let column_chunks =
-            read::read_columns_async(factory, row_group, schema.fields.clone(), None).await?;
+            read::read_columns_many_async(factory, row_group, schema.fields.clone(), None).await?;
 
         // the second step is to iterate over the columns in chunks.
         // this operation is CPU-bounded and should be sent to a separate thread pool (e.g. `tokio_rayon`) to not block
