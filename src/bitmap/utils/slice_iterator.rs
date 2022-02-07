f2aaf9c7a5051ef8789d9dca2b5612c4a44e5a54
Jorge C. Leitao <jorgecarleitao@gmail.com>
2022-02-07 07:30:03 +0000

    Optimized computation of slice from bitmap

--- src/bitmap/utils/slice_iterator.rs
+++ src/bitmap/utils/slice_iterator.rs
@@ -17,9 +17,9 @@ enum State {
 pub struct SlicesIterator<'a> {
     values: std::slice::Iter<'a, u8>,
     count: usize,
-    mask: u8,
     max_len: usize,
-    current_byte: &'a u8,
+    current_byte: u8,
+    current_mask: u8, // starts all 1, it is shifted right (e.g. may end up as 00011111)
     state: State,
     len: usize,
     start: usize,
@@ -29,23 +29,24 @@ pub struct SlicesIterator<'a> {
 impl<'a> SlicesIterator<'a> {
     /// Creates a new [`SlicesIterator`]
     pub fn new(values: &'a Bitmap) -> Self {
-        let (buffer, offset, _) = values.as_slice();
+        let (buffer, offset, length) = values.as_slice();
         let mut iter = buffer.iter();
 
         let (current_byte, state) = match iter.next() {
-            Some(b) => (b, State::Nominal),
-            None => (&0, State::Finished),
+            Some(b) => (*b >> offset, State::Nominal),
+            None => (0, State::Finished),
         };
+        let current_mask = 255u8 >> offset;
 
         Self {
             state,
             count: values.len() - values.null_count(),
-            max_len: values.len(),
+            max_len: length,
             values: iter,
-            mask: 1u8.rotate_left(offset as u32),
+            current_mask,
             current_byte,
-            len: 0,
-            start: 0,
+            len: 0,   // the length of the current region
+            start: 0, // the start of the current region
             on_region: false,
         }
     }
@@ -84,68 +85,102 @@ impl<'a> Iterator for SlicesIterator<'a> {
                 return self.finish();
             }
 
-            if self.mask == 1 {
-                // at the beginning of a byte => try to skip it all together
+            if self.current_mask == 255 {
                 match (self.on_region, self.current_byte) {
-                    (true, &255u8) => {
+                    (true, 255) => {
                         self.len = std::cmp::min(self.max_len - self.start, self.len + 8);
-                        match self.values.next() {
-                            Some(v) => self.current_byte = v,
-                            None => return self.finish(),
+                        if let Some(&v) = self.values.next() {
+                            self.current_byte = v;
                         };
                         continue;
                     }
-                    (false, &0) => {
+                    (false, 0) => {
                         self.len = std::cmp::min(self.max_len - self.start, self.len + 8);
-                        match self.values.next() {
-                            Some(v) => self.current_byte = v,
-                            None => return self.finish(),
+                        if let Some(&v) = self.values.next() {
+                            self.current_byte = v;
                         };
                         continue;
                     }
-                    _ => (), // we need to run over all bits of this byte
-                }
-            };
-
-            let value = (self.current_byte & self.mask) != 0;
-            self.mask = self.mask.rotate_left(1);
-
-            match (self.on_region, value) {
-                (true, true) => self.len += 1,
-                (false, false) => self.len += 1,
-                (true, false) => {
-                    if self.mask == 1 {
-                        // reached a new byte => try to fetch it from the iterator
-                        match self.values.next() {
-                            Some(v) => {
-                                self.on_region = false;
-                                let result = (self.start, self.len);
-                                self.start += self.len;
-                                self.len = 1;
-                                self.current_byte = v;
-                                return Some(result);
-                            }
-                            None => return self.finish(),
+                    (false, 255) => {
+                        self.on_region = true;
+                        self.start += self.len;
+                        self.len = 8;
+                        if let Some(&v) = self.values.next() {
+                            self.current_byte = v;
                         };
-                    } else {
+                        continue;
+                    }
+                    (true, 0) => {
                         self.on_region = false;
                         let result = (self.start, self.len);
                         self.start += self.len;
-                        self.len = 1;
+                        self.len = 8;
+                        if let Some(&v) = self.values.next() {
+                            self.current_byte = v;
+                        };
                         return Some(result);
                     }
-                }
-                (false, true) => {
-                    self.start += self.len;
-                    self.len = 1;
-                    self.on_region = true;
+                    _ => (),
                 }
             }
 
-            if self.mask == 1 {
+            let trailing_zeros = (self.current_byte | !self.current_mask).trailing_zeros();
+            let trailing_ones = (self.current_byte & self.current_mask).trailing_ones();
+
+            let is_valid = trailing_ones > 0;
+
+            match (self.on_region, is_valid) {
+                (true, true) => {
+                    self.len =
+                        std::cmp::min(self.max_len - self.start, self.len + trailing_ones as usize);
+                    self.current_byte >>= trailing_ones;
+                    self.current_mask >>= trailing_ones;
+                }
+                (false, false) => {
+                    self.len = std::cmp::min(
+                        self.max_len - self.start,
+                        self.len + trailing_zeros as usize,
+                    );
+                    self.current_byte >>= trailing_zeros;
+                    self.current_mask >>= trailing_zeros;
+                }
+                (true, false) => {
+                    self.on_region = false;
+                    let result = (self.start, self.len);
+                    self.start += self.len;
+                    self.len = std::cmp::min(self.max_len - self.start, trailing_zeros as usize);
+
+                    if (self.current_mask >> trailing_zeros) == 0 {
+                        // reached a new byte => try to fetch it from the iterator
+                        match self.values.next() {
+                            Some(&v) => {
+                                self.current_mask = 255u8;
+                                self.current_byte = v;
+                            }
+                            None => self.state = State::Finished,
+                        };
+                    } else {
+                        self.current_byte >>= trailing_zeros;
+                        self.current_mask >>= trailing_zeros;
+                    }
+                    return Some(result);
+                }
+                (false, true) => {
+                    self.on_region = true;
+                    self.start += self.len;
+                    self.len = std::cmp::min(self.max_len - self.start, trailing_ones as usize);
+                    self.current_byte >>= trailing_ones;
+                    self.current_mask >>= trailing_ones;
+                }
+            }
+
+            if self.current_mask == 0 {
                 // reached a new byte => try to fetch it from the iterator
                 match self.values.next() {
-                    Some(v) => self.current_byte = v,
+                    Some(&v) => {
+                        self.current_byte = v;
+                        self.current_mask = 255u8;
+                    }
                     None => return self.finish(),
                 };
             }
