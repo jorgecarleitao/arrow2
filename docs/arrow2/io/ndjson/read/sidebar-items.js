1944ee0e21ca12a198b34eb1d998161d0d637129
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2022-03-03 04:15:00 +0000

    deploy: a87c758a80facbfbcde4768ad42cdf4c5c2cc5a8

--- docs/arrow2/io/ndjson/read/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"fn":[["deserialize","Deserializes rows into an [`Array`] of [`DataType`]."],["infer","Infers the [`DataType`] from an NDJSON file, optionally only using `number_of_rows` rows."]],"struct":[["FileReader","A [`FallibleStreamingIterator`] of NDJSON rows."]],"trait":[["FallibleStreamingIterator","A fallible, streaming iterator."]]});
