26591c041b5820bf84475a18c0e7807a58a11d55
Jorge C. Leitao <jorgecarleitao@gmail.com>
2022-04-15 08:59:48 +0000

    Bumped to fixed ODBC

--- src/io/odbc/read/mod.rs
+++ src/io/odbc/read/mod.rs
@@ -5,6 +5,12 @@ mod schema;
 pub use deserialize::deserialize;
 pub use schema::infer_schema;
 
+use crate::array::Array;
+use crate::chunk::Chunk;
+use crate::datatypes::Field;
+use crate::error::Error;
+use api::Cursor;
+
 use super::api;
 
 /// Creates a [`api::buffers::ColumnarBuffer`] from the metadata.
@@ -34,4 +40,72 @@ pub fn buffer_from_metadata(
         max_batch_size,
         descs.into_iter(),
     ))
+}
+
+type CCursor<'a> = api::RowSetCursor<
+    api::CursorImpl<api::handles::StatementImpl<'a>>,
+    api::buffers::ColumnarBuffer<api::buffers::AnyColumnBuffer>,
+>;
+
+/// An iterator of [`Chunk`]s coming from an
+pub struct ChunkIterator<'a> {
+    cursor: CCursor<'a>,
+    fields: Vec<Field>,
+}
+
+impl<'a> ChunkIterator<'a> {
+    fn new(cursor: CCursor<'a>, fields: Vec<Field>) -> Self {
+        Self { cursor, fields }
+    }
+
+    /// The fields as infered from the ODBC fields
+    pub fn fields(&self) -> &[Field] {
+        &self.fields
+    }
+}
+
+impl<'a> Iterator for ChunkIterator<'a> {
+    type Item = Result<Chunk<Box<dyn Array>>, Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.cursor.fetch().transpose().map(|batch| {
+            let batch = batch?;
+            let arrays = (0..batch.num_cols())
+                .zip(self.fields.iter())
+                .map(|(index, field)| {
+                    let column_view = batch.column(index);
+                    deserialize(column_view, field.data_type.clone())
+                })
+                .collect::<Vec<_>>();
+            Ok(Chunk::new(arrays))
+        })
+    }
+}
+
+/// Returns an iterator of [`Chunk`]
+pub fn execute<'a>(
+    connection: &api::Connection<'a>,
+    query: &str,
+    params: impl api::ParameterRefCollection,
+    batch_size: Option<usize>,
+) -> Result<Option<ChunkIterator<'a>>, Error> {
+    let cursor = connection.execute(query, params)?;
+    let cursor = if let Some(cursor) = cursor {
+        cursor
+    } else {
+        return Ok(None);
+    };
+    let batch_size = batch_size.ok_or_else(|| {
+        Error::InvalidArgumentError(
+            "When execute returns data, the batch_size must be set".to_string(),
+        )
+    })?;
+
+    let fields = infer_schema(&cursor)?;
+
+    let buffer = buffer_from_metadata(&cursor, batch_size)?;
+
+    let cursor = cursor.bind_buffer(buffer)?;
+
+    Ok(Some(ChunkIterator::new(cursor, fields)))
 }
