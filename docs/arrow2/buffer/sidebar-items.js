384a14c24a361cff6f2ea729070f51b8a3ad66ef
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2022-06-01 04:29:20 +0000

    deploy: fe1e57601ef43094efb9a7a4c6b823c7cd65b4d4

--- docs/arrow2/buffer/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["Buffer","[`Buffer`] is a contiguous memory region that can be shared across thread boundaries."]]});
--- docs/arrow2/error/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"enum":[["Error","Enum with all errors in this crate."]],"type":[["Result","Typedef for a [`std::result::Result`] of an [`Error`]."]]});
