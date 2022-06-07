9b02d2f7f6a01091b4d21a036be5202ccf02c124
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2022-06-07 06:20:31 +0000

    deploy: 3136092253c15ac3edbdf5d00331ff4e982d33e1

--- docs/arrow2/io/odbc/read/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"fn":[["buffer_from_metadata","Creates a [`api::buffers::ColumnarBuffer`] from the metadata."],["deserialize","Deserializes a [`AnyColumnView`] into an array of [`DataType`]. This is CPU-bounded"],["infer_schema","Infers the Arrow [`Field`]s from a [`ResultSetMetadata`]"]]});
