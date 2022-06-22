96547fdd32072fdf7333021584c9b8854cccdc57
Jorge C. Leitao <jorgecarleitao@gmail.com>
2022-06-22 04:00:57 +0000

    Simpler

--- src/io/parquet/read/deserialize/boolean/mod.rs
+++ src/io/parquet/read/deserialize/boolean/mod.rs
@@ -1,28 +1,5 @@
 mod basic;
 mod nested;
 
-use self::nested::ArrayIterator;
-use super::{
-    nested_utils::{InitNested, NestedArrayIter},
-    DataPages,
-};
-
 pub use self::basic::Iter;
-
-/// Converts [`DataPages`] to an [`Iterator`] of [`Array`]
-pub fn iter_to_arrays_nested<'a, I: 'a>(
-    iter: I,
-    init: Vec<InitNested>,
-    chunk_size: Option<usize>,
-) -> NestedArrayIter<'a>
-where
-    I: DataPages,
-{
-    Box::new(ArrayIterator::new(iter, init, chunk_size).map(|x| {
-        x.map(|(mut nested, array)| {
-            let _ = nested.nested.pop().unwrap(); // the primitive
-            let values = array.boxed();
-            (nested, values)
-        })
-    }))
-}
+pub use nested::iter_to_arrays_nested;
--- src/io/parquet/read/deserialize/primitive/mod.rs
+++ src/io/parquet/read/deserialize/primitive/mod.rs
@@ -2,35 +2,6 @@ mod basic;
 mod dictionary;
 mod nested;
 
-pub use dictionary::DictIter;
-
-use crate::datatypes::DataType;
-
-use super::{nested_utils::*, DataPages};
-
 pub use basic::Iter;
-use nested::ArrayIterator;
-
-/// Converts [`DataPages`] to an [`Iterator`] of [`Array`]
-pub fn iter_to_arrays_nested<'a, I, T, P, F>(
-    iter: I,
-    init: Vec<InitNested>,
-    data_type: DataType,
-    chunk_size: Option<usize>,
-    op: F,
-) -> NestedArrayIter<'a>
-where
-    I: 'a + DataPages,
-    T: crate::types::NativeType,
-    P: parquet2::types::NativeType,
-    F: 'a + Copy + Send + Sync + Fn(P) -> T,
-{
-    Box::new(
-        ArrayIterator::<T, I, P, F>::new(iter, init, data_type, chunk_size, op).map(|x| {
-            x.map(|(mut nested, array)| {
-                let _ = nested.nested.pop().unwrap(); // the primitive
-                (nested, array.boxed())
-            })
-        }),
-    )
-}
+pub use dictionary::DictIter;
+pub use nested::iter_to_arrays_nested;
