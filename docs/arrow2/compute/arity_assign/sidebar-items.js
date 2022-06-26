973f8601c5dc369af1f55a663355a34db982d35f
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2022-06-26 13:45:28 +0000

    deploy: 5b66f5f33d384e2dd060281b316785ae303af5b2

---
+++ docs/arrow2/compute/arity_assign/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["binary","Applies a binary function to two [`PrimitiveArray`]s, optionally in-place, returning a new [`PrimitiveArray`]."],["unary","Applies an unary function to a [`PrimitiveArray`], optionally in-place."]]};
