33e91e77d4ce492a5c5fd545413cb63c590477eb
Ritchie Vink <ritchie46@gmail.com>
2022-06-27 10:28:06 +0200

    Commit suggestion src/io/ipc/read/common.rs
    Co-authored-by: Jorge Leitao <jorgecarleitao@gmail.com>

--- src/io/ipc/read/common.rs
+++ src/io/ipc/read/common.rs
@@ -292,7 +292,7 @@ pub fn read_dictionary<R: Read + Seek>(
     Ok(())
 }
 
-/// A small wrapper around `[Vec<u8>]` that allows us to reuse memory once it is initialized.
+/// A small wrapper around [`Vec<u8>`] that allows us to reuse memory once it is initialized.
 /// This may improve performance of the [`Read`] trait.
 #[derive(Clone, Default)]
 pub struct ReadBuffer {
