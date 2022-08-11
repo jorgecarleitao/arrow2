afdae8cd99cd73f88ee0cb94b7835805c26db2b2
Jorge C. Leitao <jorgecarleitao@gmail.com>
2022-08-11 18:23:39 +0000

    Reduced bloat

--- src/scalar/mod.rs
+++ src/scalar/mod.rs
@@ -2,7 +2,7 @@
 //! as well as concrete implementations such as [`BooleanScalar`].
 use std::any::Any;
 
-use crate::{array::*, datatypes::*};
+use crate::{array::*, datatypes::*, types::NativeType};
 
 mod dictionary;
 pub use dictionary::*;
@@ -43,43 +43,98 @@ pub trait Scalar: std::fmt::Debug + Send + Sync + dyn_clone::DynClone + 'static 
 
 dyn_clone::clone_trait_object!(Scalar);
 
-macro_rules! dyn_new_utf8 {
-    ($array:expr, $index:expr, $type:ty) => {{
-        let array = $array.as_any().downcast_ref::<Utf8Array<$type>>().unwrap();
-        let value = if array.is_valid($index) {
-            Some(array.value($index))
-        } else {
-            None
-        };
-        Box::new(Utf8Scalar::<$type>::new(value))
-    }};
+fn new_primitive<T: NativeType>(array: &PrimitiveArray<T>, index: usize) -> Box<dyn Scalar> {
+    let value = if array.is_valid(index) {
+        Some(array.value(index))
+    } else {
+        None
+    };
+    Box::new(PrimitiveScalar::new(array.data_type().clone(), value))
 }
 
-macro_rules! dyn_new_binary {
-    ($array:expr, $index:expr, $type:ty) => {{
-        let array = $array
-            .as_any()
-            .downcast_ref::<BinaryArray<$type>>()
-            .unwrap();
-        let value = if array.is_valid($index) {
-            Some(array.value($index))
-        } else {
-            None
-        };
-        Box::new(BinaryScalar::<$type>::new(value))
-    }};
+fn new_dictionary<K: DictionaryKey>(array: &DictionaryArray<K>, index: usize) -> Box<dyn Scalar> {
+    let value = if array.is_valid(index) {
+        Some(array.value(index))
+    } else {
+        None
+    };
+    Box::new(DictionaryScalar::<K>::new(array.data_type().clone(), value))
 }
 
-macro_rules! dyn_new_list {
-    ($array:expr, $index:expr, $type:ty) => {{
-        let array = $array.as_any().downcast_ref::<ListArray<$type>>().unwrap();
-        let value = if array.is_valid($index) {
-            Some(array.value($index).into())
-        } else {
-            None
-        };
-        Box::new(ListScalar::<$type>::new(array.data_type().clone(), value))
-    }};
+fn new_utf8<O: Offset>(array: &Utf8Array<O>, index: usize) -> Box<dyn Scalar> {
+    let value = if array.is_valid(index) {
+        Some(array.value(index))
+    } else {
+        None
+    };
+    Box::new(Utf8Scalar::<O>::new(value))
+}
+
+fn new_binary<O: Offset>(array: &BinaryArray<O>, index: usize) -> Box<dyn Scalar> {
+    let value = if array.is_valid(index) {
+        Some(array.value(index))
+    } else {
+        None
+    };
+    Box::new(BinaryScalar::<O>::new(value))
+}
+
+fn new_fixed_size_binary(array: &FixedSizeBinaryArray, index: usize) -> Box<dyn Scalar> {
+    let value = if array.is_valid(index) {
+        Some(array.value(index))
+    } else {
+        None
+    };
+    Box::new(FixedSizeBinaryScalar::new(array.data_type().clone(), value))
+}
+
+fn new_list<O: Offset>(array: &ListArray<O>, index: usize) -> Box<dyn Scalar> {
+    let value = if array.is_valid(index) {
+        Some(array.value(index))
+    } else {
+        None
+    };
+    Box::new(ListScalar::<O>::new(array.data_type().clone(), value))
+}
+
+fn new_fixed_size_list(array: &FixedSizeListArray, index: usize) -> Box<dyn Scalar> {
+    let value = if array.is_valid(index) {
+        Some(array.value(index))
+    } else {
+        None
+    };
+    Box::new(FixedSizeListScalar::new(array.data_type().clone(), value))
+}
+
+fn new_boolean(array: &BooleanArray, index: usize) -> Box<dyn Scalar> {
+    let value = if array.is_valid(index) {
+        Some(array.value(index))
+    } else {
+        None
+    };
+    Box::new(BooleanScalar::new(value))
+}
+
+fn new_strut(array: &StructArray, index: usize) -> Box<dyn Scalar> {
+    let value = if array.is_valid(index) {
+        let values = array
+            .values()
+            .iter()
+            .map(|x| new_scalar(x.as_ref(), index))
+            .collect();
+        Some(values)
+    } else {
+        None
+    };
+    Box::new(StructScalar::new(array.data_type().clone(), value))
+}
+
+fn new_union(array: &UnionArray, index: usize) -> Box<dyn Scalar> {
+    Box::new(UnionScalar::new(
+        array.data_type().clone(),
+        array.types()[index],
+        array.value(index),
+    ))
 }
 
 /// creates a new [`Scalar`] from an [`Array`].
@@ -87,90 +142,23 @@ pub fn new_scalar(array: &dyn Array, index: usize) -> Box<dyn Scalar> {
     use PhysicalType::*;
     match array.data_type().to_physical_type() {
         Null => Box::new(NullScalar::new()),
-        Boolean => {
-            let array = array.as_any().downcast_ref::<BooleanArray>().unwrap();
-            let value = if array.is_valid(index) {
-                Some(array.value(index))
-            } else {
-                None
-            };
-            Box::new(BooleanScalar::new(value))
-        }
+        Boolean => new_boolean(array.as_any().downcast_ref().unwrap(), index),
         Primitive(primitive) => with_match_primitive_type!(primitive, |$T| {
-            let array = array
-                .as_any()
-                .downcast_ref::<PrimitiveArray<$T>>()
-                .unwrap();
-            let value = if array.is_valid(index) {
-                Some(array.value(index))
-            } else {
-                None
-            };
-            Box::new(PrimitiveScalar::new(array.data_type().clone(), value))
+            new_primitive::<$T>(array.as_any().downcast_ref().unwrap(), index)
         }),
-        Utf8 => dyn_new_utf8!(array, index, i32),
-        LargeUtf8 => dyn_new_utf8!(array, index, i64),
-        Binary => dyn_new_binary!(array, index, i32),
-        LargeBinary => dyn_new_binary!(array, index, i64),
-        List => dyn_new_list!(array, index, i32),
-        LargeList => dyn_new_list!(array, index, i64),
-        Struct => {
-            let array = array.as_any().downcast_ref::<StructArray>().unwrap();
-            if array.is_valid(index) {
-                let values = array
-                    .values()
-                    .iter()
-                    .map(|x| new_scalar(x.as_ref(), index))
-                    .collect();
-                Box::new(StructScalar::new(array.data_type().clone(), Some(values)))
-            } else {
-                Box::new(StructScalar::new(array.data_type().clone(), None))
-            }
-        }
-        FixedSizeBinary => {
-            let array = array
-                .as_any()
-                .downcast_ref::<FixedSizeBinaryArray>()
-                .unwrap();
-            let value = if array.is_valid(index) {
-                Some(array.value(index))
-            } else {
-                None
-            };
-            Box::new(FixedSizeBinaryScalar::new(array.data_type().clone(), value))
-        }
-        FixedSizeList => {
-            let array = array.as_any().downcast_ref::<FixedSizeListArray>().unwrap();
-            let value = if array.is_valid(index) {
-                Some(array.value(index))
-            } else {
-                None
-            };
-            Box::new(FixedSizeListScalar::new(array.data_type().clone(), value))
-        }
-        Union => {
-            let array = array.as_any().downcast_ref::<UnionArray>().unwrap();
-            Box::new(UnionScalar::new(
-                array.data_type().clone(),
-                array.types()[index],
-                array.value(index),
-            ))
-        }
+        Utf8 => new_utf8::<i32>(array.as_any().downcast_ref().unwrap(), index),
+        LargeUtf8 => new_utf8::<i64>(array.as_any().downcast_ref().unwrap(), index),
+        Binary => new_binary::<i32>(array.as_any().downcast_ref().unwrap(), index),
+        LargeBinary => new_binary::<i64>(array.as_any().downcast_ref().unwrap(), index),
+        List => new_list::<i32>(array.as_any().downcast_ref().unwrap(), index),
+        LargeList => new_list::<i64>(array.as_any().downcast_ref().unwrap(), index),
+        Struct => new_strut(array.as_any().downcast_ref().unwrap(), index),
+        FixedSizeBinary => new_fixed_size_binary(array.as_any().downcast_ref().unwrap(), index),
+        FixedSizeList => new_fixed_size_list(array.as_any().downcast_ref().unwrap(), index),
+        Union => new_union(array.as_any().downcast_ref().unwrap(), index),
         Map => todo!(),
         Dictionary(key_type) => match_integer_type!(key_type, |$T| {
-            let array = array
-                .as_any()
-                .downcast_ref::<DictionaryArray<$T>>()
-                .unwrap();
-            let value = if array.is_valid(index) {
-                Some(array.value(index).into())
-            } else {
-                None
-            };
-            Box::new(DictionaryScalar::<$T>::new(
-                array.data_type().clone(),
-                value,
-            ))
+            new_dictionary::<$T>(array.as_any().downcast_ref().unwrap(), index)
         }),
     }
 }
