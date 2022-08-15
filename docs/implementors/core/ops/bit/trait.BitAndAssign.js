5e850bb574cc3e173144c784c519d9d247a702ae
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2022-08-15 19:45:21 +0000

    deploy: 3b29c8213522e1fcf5476abb836c681c5b25d394

---
+++ docs/implementors/core/ops/bit/trait.BitAndAssign.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"arrow2":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a>&lt;&amp;'a <a class=\"struct\" href=\"arrow2/bitmap/struct.Bitmap.html\" title=\"struct arrow2::bitmap::Bitmap\">Bitmap</a>&gt; for &amp;mut <a class=\"struct\" href=\"arrow2/bitmap/struct.MutableBitmap.html\" title=\"struct arrow2::bitmap::MutableBitmap\">MutableBitmap</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
