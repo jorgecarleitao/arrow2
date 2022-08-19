bc65d5172089dce4cdad7e20f2a316f2a92bac35
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2022-08-19 06:39:15 +0000

    deploy: cfb0d58e0bc8c28d0ef6bc6b130bdce8be67d64c

--- docs/implementors/std/error/trait.Error.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"arrow2":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"arrow2/error/enum.Error.html\" title=\"enum arrow2::error::Error\">Error</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
