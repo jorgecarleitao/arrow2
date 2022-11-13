edcc755f824478c9c1d42769f197bcf370bdceca
Xuanwo <github@xuanwo.io>
2022-11-13 12:37:07 +0800

    Fix deprecated warning from chrono
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- benches/cast_kernels.rs
+++ benches/cast_kernels.rs
@@ -37,7 +37,7 @@ fn build_utf8_date_array(size: usize, with_nulls: bool) -> Utf8Array<i32> {
                 None
             } else {
                 Some(
-                    NaiveDate::from_num_days_from_ce(rng.sample(range))
+                    NaiveDate::from_num_days_from_ce_opt(rng.sample(range))
                         .format("%Y-%m-%d")
                         .to_string(),
                 )
@@ -59,7 +59,7 @@ fn build_utf8_date_time_array(size: usize, with_nulls: bool) -> Utf8Array<i32> {
                 None
             } else {
                 Some(
-                    NaiveDateTime::from_timestamp(rng.sample(range), 0)
+                    NaiveDateTime::from_timestamp_opt(rng.sample(range), 0)
                         .format("%Y-%m-%dT%H:%M:%S")
                         .to_string(),
                 )
