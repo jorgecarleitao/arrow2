b94e6f64cb4bfb0cf06ff73766f68cae2445c07e
Jorge Leitao <jorgecarleitao@gmail.com>
2023-02-13 07:01:31 +0100

    Fixed writing of sliced arrays to parquet (#1397)
    * Fixed slice nested
    
    * More tests

--- src/io/parquet/write/nested/def.rs
+++ src/io/parquet/write/nested/def.rs
@@ -9,7 +9,7 @@ trait DebugIter: Iterator<Item = (u32, usize)> + std::fmt::Debug {}
 impl<A: Iterator<Item = (u32, usize)> + std::fmt::Debug> DebugIter for A {}
 
 fn single_iter<'a>(
-    validity: Option<&'a Bitmap>,
+    validity: &'a Option<Bitmap>,
     is_optional: bool,
     length: usize,
 ) -> Box<dyn DebugIter + 'a> {
@@ -26,23 +26,23 @@ fn single_iter<'a>(
     }
 }
 
-fn single_list_iter<'a, O: Offset>(nested: &ListNested<'a, O>) -> Box<dyn DebugIter + 'a> {
-    match (nested.is_optional, nested.validity) {
+fn single_list_iter<'a, O: Offset>(nested: &'a ListNested<O>) -> Box<dyn DebugIter + 'a> {
+    match (nested.is_optional, &nested.validity) {
         (false, _) => Box::new(
             std::iter::repeat(0u32)
-                .zip(to_length(nested.offsets))
+                .zip(to_length(&nested.offsets))
                 .map(|(a, b)| (a + (b != 0) as u32, b)),
         ) as Box<dyn DebugIter>,
         (true, None) => Box::new(
             std::iter::repeat(1u32)
-                .zip(to_length(nested.offsets))
+                .zip(to_length(&nested.offsets))
                 .map(|(a, b)| (a + (b != 0) as u32, b)),
         ) as Box<dyn DebugIter>,
         (true, Some(validity)) => Box::new(
             validity
                 .iter()
                 .map(|x| (x as u32))
-                .zip(to_length(nested.offsets))
+                .zip(to_length(&nested.offsets))
                 .map(|(a, b)| (a + (b != 0) as u32, b)),
         ) as Box<dyn DebugIter>,
     }
@@ -53,12 +53,12 @@ fn iter<'a>(nested: &'a [Nested]) -> Vec<Box<dyn DebugIter + 'a>> {
         .iter()
         .map(|nested| match nested {
             Nested::Primitive(validity, is_optional, length) => {
-                single_iter(*validity, *is_optional, *length)
+                single_iter(validity, *is_optional, *length)
             }
             Nested::List(nested) => single_list_iter(nested),
             Nested::LargeList(nested) => single_list_iter(nested),
             Nested::Struct(validity, is_optional, length) => {
-                single_iter(*validity, *is_optional, *length)
+                single_iter(validity, *is_optional, *length)
             }
         })
         .collect()
@@ -184,11 +184,10 @@ mod tests {
     fn struct_optional() {
         let b = [
             true, false, true, true, false, true, false, false, true, true,
-        ]
-        .into();
+        ];
         let nested = vec![
             Nested::Struct(None, true, 10),
-            Nested::Primitive(Some(&b), true, 10),
+            Nested::Primitive(Some(b.into()), true, 10),
         ];
         let expected = vec![2, 1, 2, 2, 1, 2, 1, 1, 2, 2];
 
@@ -200,7 +199,7 @@ mod tests {
         let nested = vec![
             Nested::List(ListNested {
                 is_optional: true,
-                offsets: &[0, 2],
+                offsets: vec![0, 2].try_into().unwrap(),
                 validity: None,
             }),
             Nested::Primitive(None, true, 2),
@@ -214,11 +213,10 @@ mod tests {
     fn struct_optional_1() {
         let b = [
             true, false, true, true, false, true, false, false, true, true,
-        ]
-        .into();
+        ];
         let nested = vec![
             Nested::Struct(None, true, 10),
-            Nested::Primitive(Some(&b), true, 10),
+            Nested::Primitive(Some(b.into()), true, 10),
         ];
         let expected = vec![2, 1, 2, 2, 1, 2, 1, 1, 2, 2];
 
@@ -242,7 +240,7 @@ mod tests {
             // [[0, 1], [], [2, 0, 3], [4, 5, 6], [], [7, 8, 9], [], [10]]
             Nested::List(ListNested {
                 is_optional: false,
-                offsets: &[0, 2, 2, 5, 8, 8, 11, 11, 12],
+                offsets: vec![0, 2, 2, 5, 8, 8, 11, 11, 12].try_into().unwrap(),
                 validity: None,
             }),
             Nested::Primitive(None, false, 12),
@@ -256,22 +254,21 @@ mod tests {
     fn l1_optional_optional() {
         // [[0, 1], None, [2, None, 3], [4, 5, 6], [], [7, 8, 9], None, [10]]
 
-        let v0 = [true, false, true, true, true, true, false, true].into();
+        let v0 = [true, false, true, true, true, true, false, true];
         let v1 = [
             true, true, //[0, 1]
             true, false, true, //[2, None, 3]
             true, true, true, //[4, 5, 6]
             true, true, true, //[7, 8, 9]
             true, //[10]
-        ]
-        .into();
+        ];
         let nested = vec![
             Nested::List(ListNested {
                 is_optional: true,
-                offsets: &[0, 2, 2, 5, 8, 8, 11, 11, 12],
-                validity: Some(&v0),
+                offsets: vec![0, 2, 2, 5, 8, 8, 11, 11, 12].try_into().unwrap(),
+                validity: Some(v0.into()),
             }),
-            Nested::Primitive(Some(&v1), true, 12),
+            Nested::Primitive(Some(v1.into()), true, 12),
         ];
         let expected = vec![3u32, 3, 0, 3, 2, 3, 3, 3, 3, 1, 3, 3, 3, 0, 3];
 
@@ -295,12 +292,12 @@ mod tests {
         let nested = vec![
             Nested::List(ListNested {
                 is_optional: false,
-                offsets: &[0, 2, 4],
+                offsets: vec![0, 2, 4].try_into().unwrap(),
                 validity: None,
             }),
             Nested::List(ListNested {
                 is_optional: false,
-                offsets: &[0, 3, 7, 8, 10],
+                offsets: vec![0, 3, 7, 8, 10].try_into().unwrap(),
                 validity: None,
             }),
             Nested::Primitive(None, false, 10),
@@ -312,7 +309,7 @@ mod tests {
 
     #[test]
     fn l2_optional_required_required() {
-        let a = [true, false, true, true].into();
+        let a = [true, false, true, true];
         /*
         [
             [
@@ -330,12 +327,12 @@ mod tests {
         let nested = vec![
             Nested::List(ListNested {
                 is_optional: true,
-                offsets: &[0, 2, 2, 2, 5],
-                validity: Some(&a),
+                offsets: vec![0, 2, 2, 2, 5].try_into().unwrap(),
+                validity: Some(a.into()),
             }),
             Nested::List(ListNested {
                 is_optional: false,
-                offsets: &[0, 3, 7, 8, 8, 10],
+                offsets: vec![0, 3, 7, 8, 8, 10].try_into().unwrap(),
                 validity: None,
             }),
             Nested::Primitive(None, false, 10),
@@ -347,8 +344,8 @@ mod tests {
 
     #[test]
     fn l2_optional_optional_required() {
-        let a = [true, false, true].into();
-        let b = [true, true, true, true, false].into();
+        let a = [true, false, true];
+        let b = [true, true, true, true, false];
         /*
         [
             [
@@ -366,13 +363,13 @@ mod tests {
         let nested = vec![
             Nested::List(ListNested {
                 is_optional: true,
-                offsets: &[0, 2, 2, 5],
-                validity: Some(&a),
+                offsets: vec![0, 2, 2, 5].try_into().unwrap(),
+                validity: Some(a.into()),
             }),
             Nested::List(ListNested {
                 is_optional: true,
-                offsets: &[0, 3, 7, 8, 8, 8],
-                validity: Some(&b),
+                offsets: vec![0, 3, 7, 8, 8, 8].try_into().unwrap(),
+                validity: Some(b.into()),
             }),
             Nested::Primitive(None, false, 8),
         ];
@@ -383,9 +380,9 @@ mod tests {
 
     #[test]
     fn l2_optional_optional_optional() {
-        let a = [true, false, true].into();
-        let b = [true, true, true, false].into();
-        let c = [true, true, true, true, false, true, true, true].into();
+        let a = [true, false, true];
+        let b = [true, true, true, false];
+        let c = [true, true, true, true, false, true, true, true];
         /*
         [
             [
@@ -402,15 +399,15 @@ mod tests {
         let nested = vec![
             Nested::List(ListNested {
                 is_optional: true,
-                offsets: &[0, 2, 2, 4],
-                validity: Some(&a),
+                offsets: vec![0, 2, 2, 4].try_into().unwrap(),
+                validity: Some(a.into()),
             }),
             Nested::List(ListNested {
                 is_optional: true,
-                offsets: &[0, 3, 7, 8, 8],
-                validity: Some(&b),
+                offsets: vec![0, 3, 7, 8, 8].try_into().unwrap(),
+                validity: Some(b.into()),
             }),
-            Nested::Primitive(Some(&c), true, 8),
+            Nested::Primitive(Some(c.into()), true, 8),
         ];
         let expected = vec![5, 5, 5, 5, 4, 5, 5, 0, 5, 2];
 
@@ -431,21 +428,19 @@ mod tests {
     fn nested_list_struct_nullable() {
         let a = [
             true, true, true, false, true, false, false, false, true, true, true, true,
-        ]
-        .into();
+        ];
         let b = [
             true, true, true, false, true, true, true, true, true, true, true, true,
-        ]
-        .into();
-        let c = [true, false, true, true, true, true, false, true].into();
+        ];
+        let c = [true, false, true, true, true, true, false, true];
         let nested = vec![
             Nested::List(ListNested {
                 is_optional: true,
-                offsets: &[0, 2, 2, 5, 8, 8, 11, 11, 12],
-                validity: Some(&c),
+                offsets: vec![0, 2, 2, 5, 8, 8, 11, 11, 12].try_into().unwrap(),
+                validity: Some(c.into()),
             }),
-            Nested::Struct(Some(&b), true, 12),
-            Nested::Primitive(Some(&a), true, 12),
+            Nested::Struct(Some(b.into()), true, 12),
+            Nested::Primitive(Some(a.into()), true, 12),
         ];
         let expected = vec![4, 4, 0, 4, 2, 4, 3, 3, 3, 1, 4, 4, 4, 0, 4];
 
@@ -454,12 +449,12 @@ mod tests {
 
     #[test]
     fn nested_list_struct_nullable1() {
-        let c = [true, false].into();
+        let c = [true, false];
         let nested = vec![
             Nested::List(ListNested {
                 is_optional: true,
-                offsets: &[0, 1, 1],
-                validity: Some(&c),
+                offsets: vec![0, 1, 1].try_into().unwrap(),
+                validity: Some(c.into()),
             }),
             Nested::Struct(None, true, 1),
             Nested::Primitive(None, true, 1),
@@ -471,19 +466,18 @@ mod tests {
 
     #[test]
     fn nested_struct_list_nullable() {
-        let a = [true, false, true, true, true, true, false, true].into();
+        let a = [true, false, true, true, true, true, false, true];
         let b = [
             true, true, true, false, true, true, true, true, true, true, true, true,
-        ]
-        .into();
+        ];
         let nested = vec![
             Nested::Struct(None, true, 12),
             Nested::List(ListNested {
                 is_optional: true,
-                offsets: &[0, 2, 2, 5, 8, 8, 11, 11, 12],
-                validity: Some(&a),
+                offsets: vec![0, 2, 2, 5, 8, 8, 11, 11, 12].try_into().unwrap(),
+                validity: Some(a.into()),
             }),
-            Nested::Primitive(Some(&b), true, 12),
+            Nested::Primitive(Some(b.into()), true, 12),
         ];
         let expected = vec![4, 4, 1, 4, 3, 4, 4, 4, 4, 2, 4, 4, 4, 1, 4];
 
@@ -492,13 +486,13 @@ mod tests {
 
     #[test]
     fn nested_struct_list_nullable1() {
-        let a = [true, true, false].into();
+        let a = [true, true, false];
         let nested = vec![
             Nested::Struct(None, true, 3),
             Nested::List(ListNested {
                 is_optional: true,
-                offsets: &[0, 1, 1, 1],
-                validity: Some(&a),
+                offsets: vec![0, 1, 1, 1].try_into().unwrap(),
+                validity: Some(a.into()),
             }),
             Nested::Primitive(None, true, 1),
         ];
@@ -515,23 +509,23 @@ mod tests {
         ]
         */
 
-        let a = [true].into();
-        let b = [true, false].into();
-        let c = [true, false].into();
-        let d = [true].into();
+        let a = [true];
+        let b = [true, false];
+        let c = [true, false];
+        let d = [true];
         let nested = vec![
             Nested::List(ListNested {
                 is_optional: true,
-                offsets: &[0, 2],
-                validity: Some(&a),
+                offsets: vec![0, 2].try_into().unwrap(),
+                validity: Some(a.into()),
             }),
-            Nested::Struct(Some(&b), true, 2),
+            Nested::Struct(Some(b.into()), true, 2),
             Nested::List(ListNested {
                 is_optional: true,
-                offsets: &[0, 1, 1],
-                validity: Some(&c),
+                offsets: vec![0, 1, 1].try_into().unwrap(),
+                validity: Some(c.into()),
             }),
-            Nested::Primitive(Some(&d), true, 1),
+            Nested::Primitive(Some(d.into()), true, 1),
         ];
         /*
                 0 6
@@ -559,29 +553,29 @@ mod tests {
             [{"a": []}],
         ]
             */
-        let a = [true, false, true, true, true, true, false, true].into();
+        let a = [true, false, true, true, true, true, false, true];
         let b = [
             true, true, true, false, true, true, true, true, true, true, true, true,
-        ]
-        .into();
+        ];
         let c = [
             true, true, true, false, true, false, false, false, true, true, true, true,
-        ]
-        .into();
-        let d = [true, true, true, true, true, false, true, true].into();
+        ];
+        let d = [true, true, true, true, true, false, true, true];
         let nested = vec![
             Nested::List(ListNested {
                 is_optional: true,
-                offsets: &[0, 2, 2, 5, 8, 8, 11, 11, 12],
-                validity: Some(&a),
+                offsets: vec![0, 2, 2, 5, 8, 8, 11, 11, 12].try_into().unwrap(),
+                validity: Some(a.into()),
             }),
-            Nested::Struct(Some(&b), true, 12),
+            Nested::Struct(Some(b.into()), true, 12),
             Nested::List(ListNested {
                 is_optional: true,
-                offsets: &[0, 1, 2, 3, 3, 4, 4, 4, 4, 5, 6, 8, 8],
-                validity: Some(&c),
+                offsets: vec![0, 1, 2, 3, 3, 4, 4, 4, 4, 5, 6, 8, 8]
+                    .try_into()
+                    .unwrap(),
+                validity: Some(c.into()),
             }),
-            Nested::Primitive(Some(&d), true, 8),
+            Nested::Primitive(Some(d.into()), true, 8),
         ];
         let expected = vec![6, 6, 0, 6, 2, 6, 3, 3, 3, 1, 6, 5, 6, 6, 0, 4];
 
