95e9b389dea628c2a299126af4fcfd12d9fe1bf5
Özgür Akkurt <91746947+ozgrakkurt@users.noreply.github.com>
2023-02-24 23:49:33 +0300

    Re-exported the `bloom_filter` module from `parquet2` crate (#1420)

--- src/io/parquet/mod.rs
+++ src/io/parquet/mod.rs
@@ -4,6 +4,10 @@ use crate::error::Error;
 pub mod read;
 pub mod write;
 
+#[cfg(feature = "io_parquet_bloom_filter")]
+#[cfg_attr(docsrs, doc(cfg(feature = "io_parquet_bloom_filter")))]
+pub use parquet2::bloom_filter;
+
 const ARROW_SCHEMA_META_KEY: &str = "ARROW:schema";
 
 impl From<parquet2::error::Error> for Error {
