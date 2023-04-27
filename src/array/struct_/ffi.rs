9f171f8f019fa4a2e1d38d89c4c34a0ca096a458
ritchie <ritchie46@gmail.com>
2023-04-27 10:26:12 +0200

    Fixed struct FFI for sliced pyarrow

--- src/array/struct_/ffi.rs
+++ src/array/struct_/ffi.rs
@@ -30,11 +30,38 @@ impl<A: ffi::ArrowArrayRef> FromFfi<A> for StructArray {
         let data_type = array.data_type().clone();
         let fields = Self::get_fields(&data_type);
 
+        let arrow_array = array.array();
         let validity = unsafe { array.validity() }?;
+        let len = arrow_array.len();
+        let offset = arrow_array.offset();
         let values = (0..fields.len())
             .map(|index| {
                 let child = array.child(index)?;
-                ffi::try_from(child)
+                ffi::try_from(child).map(|arr| {
+                    // there is a discrepancy with how arrow2 exports sliced
+                    // struct array and how pyarrow does it.
+                    // # Pyarrow
+                    // ## struct array len 3
+                    //  * slice 1 by with len 2
+                    //      offset on struct array: 1
+                    //      length on struct array: 2
+                    //      offset on value array: 0
+                    //      length on value array: 3
+                    // # Arrow2
+                    // ## struct array len 3
+                    //  * slice 1 by with len 2
+                    //      offset on struct array: 0
+                    //      length on struct array: 3
+                    //      offset on value array: 1
+                    //      length on value array: 2
+                    //
+                    // this branch will ensure both can round trip
+                    if arr.len() >= (len + offset) {
+                        arr.sliced(offset, len)
+                    } else {
+                        arr
+                    }
+                })
             })
             .collect::<Result<Vec<Box<dyn Array>>>>()?;
 
