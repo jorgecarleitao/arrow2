d96d2a449872a07b1cf6eae1be3a13ebbbdc1617
ritchie46 <ritchie46@users.noreply.github.com>
2023-05-15 17:54:57 +0000

    deploy: ef2ef5e1135e35e262d105a7ab93813dbbf732b2

--- main/docs/arrow2/compute/hash/sidebar-items.js
+++ main/docs/arrow2/compute/hash/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["can_hash","hash","hash_binary","hash_boolean","hash_primitive","hash_utf8"],"mod":["__multiversion"]};
+window.SIDEBAR_ITEMS = {"fn":["can_hash","hash","hash_binary","hash_boolean","hash_primitive","hash_utf8"]};
