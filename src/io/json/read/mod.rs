148ea37467dc8d3375a23deab7c2087c630d4c87
Ritchie Vink <ritchie46@gmail.com>
2023-05-20 13:57:51 +0200

    Splitted 'io_json' feature flags in read/write (#1487)

--- src/io/json/read/mod.rs
+++ src/io/json/read/mod.rs
@@ -8,3 +8,11 @@ pub(crate) use infer_schema::coerce_data_type;
 pub use infer_schema::{infer, infer_records_schema};
 
 pub use json_deserializer;
+
+use crate::error::Error;
+
+impl From<json_deserializer::Error> for Error {
+    fn from(error: json_deserializer::Error) -> Self {
+        Error::ExternalFormat(error.to_string())
+    }
+}
