7c7d7028c70177828a1e37ccd2a73c3a49c984b6
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2023-09-05 11:26:43 -0700

    lints

--- src/io/parquet/read/schema/convert.rs
+++ src/io/parquet/read/schema/convert.rs
@@ -172,7 +172,10 @@ fn from_fixed_len_byte_array(
 fn to_primitive_type_inner(primitive_type: &PrimitiveType) -> DataType {
     // Unknown type refers to values that should always be treated as Null
     // See: https://github.com/apache/parquet-format/blob/master/LogicalTypes.md#unknown-always-null
-    if matches!(primitive_type.logical_type, Some(PrimitiveLogicalType::Unknown)) {
+    if matches!(
+        primitive_type.logical_type,
+        Some(PrimitiveLogicalType::Unknown)
+    ) {
         return DataType::Null;
     }
 
