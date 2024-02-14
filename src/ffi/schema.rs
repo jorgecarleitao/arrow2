pola-rs/polars

feat: implement ffi for `binview` (#13871)

crates/polars-arrow/src/ffi/schema.rs
@@ -271,6 +271,8 @@ unsafe fn to_data_type(schema: &ArrowSchema) -> PolarsResult<ArrowDataType> {
         "tDn" => ArrowDataType::Duration(TimeUnit::Nanosecond),
         "tiM" => ArrowDataType::Interval(IntervalUnit::YearMonth),
         "tiD" => ArrowDataType::Interval(IntervalUnit::DayTime),
+        "vu" => ArrowDataType::Utf8View,
+        "vz" => ArrowDataType::BinaryView,
         "+l" => {
             let child = schema.child(0);
             ArrowDataType::List(Box::new(to_field(child)?))

jorgecarleitao/arrow2

src/ffi/schema.rs
@@ -261,6 +261,8 @@ unsafe fn to_data_type(schema: &ArrowSchema) -> Result<DataType> {
         "tDn" => DataType::Duration(TimeUnit::Nanosecond),
         "tiM" => DataType::Interval(IntervalUnit::YearMonth),
         "tiD" => DataType::Interval(IntervalUnit::DayTime),
+        "vu" => DataType::Utf8View,
+        "vz" => DataType::BinaryView,
         "+l" => {
             let child = schema.child(0);
             DataType::List(Box::new(to_field(child)?))